a2c2ef68fdabef8888993f34ded0d828481de447
brandoncimino <brandon.cimino@gmail.com>
2020-09-22 14:58:22 -0500

    Added BrandonUtils copy from fortune-fountain-g project

---
+++ Packages/BrandonUtils/Runtime/Saving/SaveDataTestImpl.cs
@@ -0,0 +1,11 @@
+﻿namespace Packages.BrandonUtils.Runtime.Saving {
+    /// <summary>
+    ///     An example implementation of <see cref="SaveData{T}"/>.
+    /// </summary>
+    /// <remarks>
+    ///     Intended only for use by unit tests.
+    /// </remarks>
+    public class SaveDataTestImpl : SaveData<SaveDataTestImpl> {
+        public string Word;
+    }
+}
