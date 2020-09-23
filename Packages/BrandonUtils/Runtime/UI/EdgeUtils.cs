df0518d9393e0a762c26491287735a0b0b6b457f
brandoncimino <brandon.cimino@gmail.com>
2020-09-23 12:30:05 -0500

    Added comments to EdgeUtils.cs

--- Packages/BrandonUtils/Runtime/UI/EdgeUtils.cs
+++ Packages/BrandonUtils/Runtime/UI/EdgeUtils.cs
@@ -1,9 +1,18 @@
-﻿using Packages.BrandonUtils.Runtime.Enums;
+﻿using System;
+using System.ComponentModel;
+
+using Packages.BrandonUtils.Runtime.Enums;
 
 using UnityEngine;
 
 namespace Packages.BrandonUtils.Runtime.UI {
     public static class EdgeUtils {
+        /// <summary>
+        /// Gets the relevant <see cref="RectTransform.Axis"/> for <paramref name="edge"/>, i.e. <see cref="RectTransform.Edge.Bottom"/> -> <see cref="RectTransform.Axis.Vertical"/>.
+        /// </summary>
+        /// <param name="edge"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidEnumArgumentException"></exception>
         public static RectTransform.Axis Axis(this RectTransform.Edge edge) {
             switch (edge) {
                 case RectTransform.Edge.Left:
@@ -17,6 +26,12 @@ namespace Packages.BrandonUtils.Runtime.UI {
             }
         }
 
+        /// <summary>
+        /// Returns the <see cref="RectTransform.Edge"/> opposite of <paramref name="edge"/>, i.e. <see cref="RectTransform.Edge.Bottom"/> -> <see cref="RectTransform.Edge.Top"/>.
+        /// </summary>
+        /// <param name="edge"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidEnumArgumentException"></exception>
         public static RectTransform.Edge Inverse(this RectTransform.Edge edge) {
             switch (edge) {
                 case RectTransform.Edge.Left:
@@ -32,6 +47,12 @@ namespace Packages.BrandonUtils.Runtime.UI {
             }
         }
 
+        /// <summary>
+        /// Returns the "<see cref="Math.Sign(decimal)"/>" of <paramref name="edge"/> along its <see cref="Axis"/>, i.e. <see cref="RectTransform.Edge.Left"/> -> -1; <see cref="RectTransform.Edge.Right"/> -> 1.
+        /// </summary>
+        /// <param name="edge"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidEnumArgumentException"></exception>
         public static int Direction(this RectTransform.Edge edge) {
             switch (edge) {
                 case RectTransform.Edge.Left:
