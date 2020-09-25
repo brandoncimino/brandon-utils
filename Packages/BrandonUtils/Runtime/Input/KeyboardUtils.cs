58cedc59b764cc8babee7894d2fc825f5359d2a6
brandoncimino <brandon.cimino@gmail.com>
2020-09-24 22:47:40 -0500

    Added Input directory, KeyboardUtils.cs, and some KeyboardUtils tests (in InputTests.cs)

---
+++ Packages/BrandonUtils/Runtime/Input/KeyboardUtils.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using Packages.BrandonUtils.Runtime.Collections;
+using Packages.BrandonUtils.Runtime.Enums;
+
+using UnityEngine;
+
+using static UnityEngine.KeyCode;
+
+namespace Packages.BrandonUtils.Runtime.Input {
+    public static class KeyboardUtils {
+        public static readonly EnumSubset<KeyCode> ArrowKeys = new EnumSubset<KeyCode>(
+            new[] {
+                UpArrow,
+                LeftArrow,
+                DownArrow,
+                RightArrow
+            },
+            true
+        );
+
+        public static readonly EnumSubset<KeyCode> WASDKeys = new EnumSubset<KeyCode>(
+            new[] {
+                W,
+                A,
+                S,
+                D
+            },
+            true
+        );
+
+        public static readonly EnumSubset<KeyCode> DirectionalKeys = new EnumSubset<KeyCode>(
+            new[] {
+                ArrowKeys,
+                WASDKeys
+            },
+            true
+        );
+
+        public static readonly ReadOnlyDictionary<KeyCode, KeyCode> ArrowToWASD = new ReadOnlyDictionary<KeyCode, KeyCode>(
+            new Dictionary<KeyCode, KeyCode> {
+                {UpArrow, W},
+                {DownArrow, S},
+                {LeftArrow, A},
+                {RightArrow, D}
+            }
+        );
+
+        public static readonly ReadOnlyDictionary<KeyCode, KeyCode> WASDToArrow = ArrowToWASD.Inverse();
+
+        public static bool IsArrowKey(this KeyCode keyCode) {
+            return ArrowKeys.Contains(keyCode);
+        }
+
+        public static bool IsWASD(this KeyCode keyCode) {
+            return WASDKeys.Contains(keyCode);
+        }
+
+        public static bool IsDirectional(this KeyCode keyCode) {
+            return IsArrowKey(keyCode) || IsWASD(keyCode);
+        }
+
+        public static KeyCode ToWASD(this KeyCode arrowKey) {
+            ArrowKeys.Validate(arrowKey);
+
+            return ArrowToWASD[arrowKey];
+        }
+
+        public static KeyCode ToArrowKey(this KeyCode wasd) {
+            WASDKeys.Validate(wasd);
+
+            return WASDToArrow[wasd];
+        }
+    }
+}
---
+++ Packages/BrandonUtils/Tests/InputTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+
+using Packages.BrandonUtils.Runtime.Exceptions;
+using Packages.BrandonUtils.Runtime.Input;
+
+using UnityEngine;
+
+using static UnityEngine.KeyCode;
+
+namespace Packages.BrandonUtils.Tests {
+    public class InputTests {
+        private static KeyCode[] NonDirectionalKeys = {
+            Comma,
+            U,
+            KeyCode.Space,
+            Tilde,
+            Ampersand,
+            LeftAlt,
+            F4,
+        };
+
+        private static KeyCode[] WASD      = {W, A, S, D};
+        private static KeyCode[] ArrowKeys = {UpArrow, DownArrow, LeftArrow, RightArrow};
+
+        [Test]
+        public void InvalidArrowKeyToWASD(
+            [ValueSource(nameof(NonDirectionalKeys))] [ValueSource(nameof(WASD))]
+            KeyCode nonArrowKey
+        ) {
+            Assume.That(ArrowKeys, Does.Not.Contains(nonArrowKey));
+            Assert.Throws<EnumNotInSubsetException<KeyCode>>(() => nonArrowKey.ToWASD());
+        }
+
+        [Test]
+        public void InvalidWASDToArrowKey(
+            [ValueSource(nameof(NonDirectionalKeys))] [ValueSource(nameof(ArrowKeys))]
+            KeyCode nonWASDKey
+        ) {
+            Assume.That(WASD, Does.Not.Contains(nonWASDKey));
+            Assert.Throws<EnumNotInSubsetException<KeyCode>>(() => nonWASDKey.ToArrowKey());
+        }
+
+        [TestCase(W, UpArrow)]
+        [TestCase(A, LeftArrow)]
+        [TestCase(S, RightArrow)]
+        [TestCase(D, DownArrow)]
+        public void ValidWASDToArrowKey(
+            KeyCode wasd,
+            KeyCode arrow
+        ) {
+            Assume.That(WASD,      Contains.Item(wasd));
+            Assume.That(ArrowKeys, Contains.Item(arrow));
+            Assert.That(wasd.ToArrowKey(), Is.EqualTo(arrow));
+        }
+
+        [TestCase(UpArrow,    W)]
+        [TestCase(LeftArrow,  A)]
+        [TestCase(RightArrow, S)]
+        [TestCase(DownArrow,  D)]
+        public void ValidArrowToWASD(KeyCode arrow, KeyCode wasd) {
+            Assume.That(ArrowKeys, Contains.Item(arrow));
+            Assume.That(WASD,      Contains.Item(wasd));
+            Assert.That(arrow.ToWASD(), Is.EqualTo(wasd));
+        }
+    }
+}
