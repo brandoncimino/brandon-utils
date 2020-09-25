b4be4029113df8fc1ba470a3d5524c3b7cc1858e
brandoncimino <brandon.cimino@gmail.com>
2020-09-24 20:56:02 -0500

    Added EnumSubset.cs and an accompanying exception, EnumNotInSubsetException.cs

---
+++ Packages/BrandonUtils/Runtime/Enums/EnumSubset.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using JetBrains.Annotations;
+
+using Packages.BrandonUtils.Runtime.Exceptions;
+
+namespace Packages.BrandonUtils.Runtime.Enums {
+    public class EnumSubset<T> : IList<T> where T : Enum {
+        /// <summary>
+        /// The backing <see cref="List{T}"/> for the <see cref="EnumSubset{T}"/>.
+        /// </summary>
+        private readonly List<T> _values;
+
+        #region Constructors
+
+        [UsedImplicitly]
+        public EnumSubset(IEnumerable<T> values) {
+            _values = values.ToList();
+        }
+
+        public EnumSubset(params T[] values) : this((IEnumerable<T>) values) { }
+
+        #endregion
+
+        #region IList Delegated Implementation
+
+        public IEnumerator<T> GetEnumerator() {
+            return _values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return _values.GetEnumerator();
+        }
+
+        public void Add(T item) {
+            _values.Add(item);
+        }
+
+        public void Clear() {
+            _values.Clear();
+        }
+
+        public bool Contains(T item) {
+            return _values.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex) {
+            _values.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item) {
+            return _values.Remove(item);
+        }
+
+        public int  Count      => _values.Count;
+        public bool IsReadOnly { get; } = false;
+
+        public int IndexOf(T item) {
+            return _values.IndexOf(item);
+        }
+
+        public void Insert(int index, T item) {
+            _values.Insert(index, item);
+        }
+
+        public void RemoveAt(int index) {
+            _values.RemoveAt(index);
+        }
+
+        public T this[int index] {
+            get => _values[index];
+            set => _values[index] = value;
+        }
+
+        #endregion
+
+        #region Meaningful Stuff
+
+        /// <summary>
+        /// Checks if <see cref="_values"/> <see cref="Contains"/> <paramref name="valuesToValidate"/>.
+        /// </summary>
+        /// <remarks>
+        /// This should only be used when you want strict validation that throws an <see cref="Exception"/> on failure.
+        /// <br/>If you want to "test" whether or not a value is in an <see cref="EnumSubset{T}"/>, use <see cref="Contains"/>.
+        /// </remarks>
+        /// <param name="valuesToValidate">The <see cref="T"/> <see cref="Enum"/> value that should be within this <see cref="EnumSubset{T}"/>.</param>
+        /// <returns><paramref name="valuesToValidate"/>, if <see cref="Contains">Contains</see>(<paramref name="valuesToValidate"/>) returns <c>true</c>.</returns>
+        /// <exception cref="EnumNotInSubsetException{T}">if <see cref="Contains">Contains</see>(<paramref name="valuesToValidate"/>) returns <c>false</c>.</exception>
+        public void Validate(params T[] valuesToValidate) {
+            if (!valuesToValidate.All(Contains)) {
+                throw new EnumNotInSubsetException<T>(
+                    this,
+                    valuesToValidate
+                );
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Packages/BrandonUtils/Runtime/Exceptions/EnumNotInSubsetException.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+using Packages.BrandonUtils.Runtime.Collections;
+
+namespace Packages.BrandonUtils.Runtime.Exceptions {
+    public class EnumNotInSubsetException<T> : InvalidEnumArgumentException where T : Enum {
+        public override string Message { get; }
+
+        /// <inheritdoc cref="InvalidEnumArgumentException"/>
+        /// <summary>
+        /// Constructs a new <see cref="EnumNotInSubsetException{T}"/>, listing information about the invalid <paramref name="values"/> and the <paramref name="subset"/>.
+        /// </summary>
+        /// <param name="values">A collection of <see cref="T"/> values, where <b>at least one</b> (but not necessarily all) is <b>not</b> in <paramref name="subset"/>.
+        /// <br/>
+        /// Only unique, invalid items from <paramref name="values"/> will be included in the logging message.
+        /// </param>
+        /// <param name="subset">The set of valid <see cref="T"/> values.</param>
+        /// <param name="message">A user-provided message, which will be <b>prepended</b> to the built-in message.</param>
+        /// <param name="innerException">The <see cref="Exception"/> that caused this, if any.</param>
+        public EnumNotInSubsetException(
+            ICollection<T> subset,
+            IEnumerable<T> values = null,
+            string message = null,
+            Exception innerException = null
+        ) : base(message, innerException) {
+            var badValues = (values ?? Enumerable.Empty<T>()).Where(value => !subset.Contains(value)).ToArray();
+
+            var badValueString = badValues.Length == 0 ? null : $"Invalid values: [{badValues.JoinString(", ")}]";
+
+            var subsetString = $"Allowed values: [{subset.JoinString(", ")}]";
+
+            var lines = new[] {
+                message,
+                badValueString,
+                subsetString
+            };
+
+            Message = lines.Where(ln => ln != null).JoinString("\n");
+        }
+
+        public EnumNotInSubsetException(
+            ICollection<T> subset,
+            T invalidValue,
+            string message = null,
+            Exception innerException = null
+        ) : this(subset, Enumerable.Repeat(invalidValue, 1), message, innerException) { }
+    }
+}
