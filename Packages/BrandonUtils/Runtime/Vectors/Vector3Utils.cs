172139f52f2dca0f765ae423c592eb737580393b
brandoncimino <brandon.cimino@gmail.com>
2020-10-03 18:35:27 -0500

    Added Vector3Utils.cs

---
+++ Packages/BrandonUtils/Runtime/Vectors/Vector3Utils.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Packages.BrandonUtils.Runtime.Vectors {
+    public static class Vector3Utils {
+        public static Vector3 Copy(this Vector3 original) {
+            return new Vector3(original.x, original.y, original.z);
+        }
+
+        /// <summary>
+        /// Multiplies each component of <paramref name="original"/> by the corresponding component of <paramref name="scalar"/>.
+        /// </summary>
+        /// <remarks>
+        /// This is similar to <see cref="Vector3.Scale(UnityEngine.Vector3)"/>, but returns a <b>new <see cref="Vector3"/></b> rather than modifying <paramref name="original"/>.
+        /// </remarks>
+        /// <param name="original"></param>
+        /// <param name="scalar"></param>
+        /// <returns></returns>
+        public static Vector3 Scaled(this Vector3 original, Vector3 scalar) {
+            return Vector3.Scale(original, scalar);
+        }
+    }
+}
