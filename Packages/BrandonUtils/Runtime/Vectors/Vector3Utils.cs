1feb2b2d31072685f0982d13e0670bfbbfe2f0c3
brandoncimino <brandon.cimino@gmail.com>
2020-10-04 12:48:04 -0500

    Added aviation axis aliases

--- Packages/BrandonUtils/Runtime/Vectors/Vector3Utils.cs
+++ Packages/BrandonUtils/Runtime/Vectors/Vector3Utils.cs
@@ -18,5 +18,36 @@ namespace Packages.BrandonUtils.Runtime.Vectors {
         public static Vector3 Scaled(this Vector3 original, Vector3 scale) {
             return Vector3.Scale(original, scale);
         }
+
+        #region Aviation Axes
+
+        /// <summary>
+        /// An "alias" for <paramref name="eulerAngles"/>.<see cref="Vector3.x"/> which returns the <a href="https://en.wikipedia.org/wiki/Aircraft_principal_axes#Transverse_axis_(pitch)">aviation "pitch" (transverse)</a> axis.
+        /// </summary>
+        /// <param name="eulerAngles"></param>
+        /// <returns></returns>
+        public static float Pitch(this Vector3 eulerAngles) {
+            return eulerAngles.x;
+        }
+
+        /// <summary>
+        /// An "alias" for <paramref name="eulerAngles"/>.<see cref="Vector3.y"/> which returns the <a href="https://en.wikipedia.org/wiki/Aircraft_principal_axes#Vertical_axis_(yaw)">aviation "yaw" (vertical)</a> axis.
+        /// </summary>
+        /// <param name="eulerAngles"></param>
+        /// <returns></returns>
+        public static float Yaw(this Vector3 eulerAngles) {
+            return eulerAngles.y;
+        }
+
+        /// <summary>
+        /// An "alias" for <paramref name="eulerAngles"/>.<see cref="Vector3.y"/> which returns the <a href="https://en.wikipedia.org/wiki/Aircraft_principal_axes#Longitudinal_axis_(roll)">aviation "roll" (longitudinal)</a> axis.
+        /// </summary>
+        /// <param name="eulerAngles"></param>
+        /// <returns></returns>
+        public static float Roll(this Vector3 eulerAngles) {
+            return eulerAngles.z;
+        }
+
+        #endregion
     }
 }
