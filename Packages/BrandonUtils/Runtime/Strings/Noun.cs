a93c0122f409b12dce5d17d9b2ee1f83e9cda646
brandoncimino <brandon.cimino@gmail.com>
2020-10-06 16:58:39 -0500

    formatting of noun apparently

--- Packages/BrandonUtils/Runtime/Strings/Noun.cs
+++ Packages/BrandonUtils/Runtime/Strings/Noun.cs
@@ -1,21 +1,17 @@
-﻿namespace Packages.BrandonUtils.Runtime
-{
+﻿namespace Packages.BrandonUtils.Runtime {
     /// <summary>
     /// A class for handling noun conjugations consistently.
     /// </summary>
-    public class Noun
-    {
+    public class Noun {
         public readonly string Singular;
         public readonly string Plural;
 
-        public Noun(string singular, string plural = null)
-        {
+        public Noun(string singular, string plural = null) {
             this.Singular = singular;
-            this.Plural = plural ?? Singular + "s";
+            this.Plural   = plural ?? Singular + "s";
         }
 
-        public string Get(int quantity = 1)
-        {
+        public string Get(int quantity = 1) {
             return quantity == 1 ? Singular : Plural;
         }
     }
