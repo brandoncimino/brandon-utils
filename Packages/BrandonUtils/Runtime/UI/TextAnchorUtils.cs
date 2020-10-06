ceb1890f10214b75e5fff74efa52e292f6926de6
brandoncimino <brandon.cimino@gmail.com>
2020-10-06 16:23:41 -0500

    Added TextAnchorUtils.cs

---
+++ Packages/BrandonUtils/Runtime/UI/TextAnchorUtils.cs
@@ -0,0 +1,34 @@
+﻿using Packages.BrandonUtils.Runtime.Enums;
+
+using UnityEngine;
+
+using Vector2 = System.Numerics.Vector2;
+
+namespace Packages.BrandonUtils.Runtime.UI {
+    public static class TextAnchorUtils {
+        public static Vector2 Anchor(this TextAnchor textAnchor) {
+            switch (textAnchor) {
+                case TextAnchor.UpperLeft:
+                    return new Vector2(0, 1);
+                case TextAnchor.UpperCenter:
+                    return new Vector2(0.1f, 1);
+                case TextAnchor.UpperRight:
+                    return new Vector2(1, 1);
+                case TextAnchor.MiddleLeft:
+                    return new Vector2(0, 0.5f);
+                case TextAnchor.MiddleCenter:
+                    return new Vector2(0.5f, 0.5f);
+                case TextAnchor.MiddleRight:
+                    return new Vector2(1, 0.5f);
+                case TextAnchor.LowerLeft:
+                    return new Vector2(0, 0);
+                case TextAnchor.LowerCenter:
+                    return new Vector2(0.5f, 0);
+                case TextAnchor.LowerRight:
+                    return new Vector2(1, 0);
+                default:
+                    throw EnumUtils.InvalidEnumArgumentException(nameof(textAnchor), textAnchor);
+            }
+        }
+    }
+}
