4509bfafd878fac5fc0e5d736a1fc4f18b16b649
brandoncimino <brandon.cimino@gmail.com>
2020-10-06 16:57:54 -0500

    Moved Noun into /Strings Added Author.cs and Hyperlink.cs

---
+++ Packages/BrandonUtils/Runtime/Strings/Author.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Packages.BrandonUtils.Runtime.Collections;
+
+namespace Packages.BrandonUtils.Runtime {
+    public class Author {
+        public List<string> NameParts;
+        public Hyperlink    Website;
+
+        public string FullName => NameParts.Where(n => !string.IsNullOrWhiteSpace(n)).JoinString(" ");
+
+        public string Citation(Hyperlink.MarkupLanguage markupLanguage = Hyperlink.MarkupLanguage.HTML) {
+            return $"{FullName} ({Website.ToString(markupLanguage)})";
+        }
+    }
+}
