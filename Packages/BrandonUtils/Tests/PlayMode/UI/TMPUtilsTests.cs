165e7db38c6f987b14e39488fb3ab4c5697cfbb2
brandoncimino <brandon.cimino@gmail.com>
2021-01-17 00:22:57 -0600

    Added the first TMPUtilsTests.cs test

--- Packages/BrandonUtils/Tests/PlayMode/UI/TMPUtilsTests.cs
+++ Packages/BrandonUtils/Tests/PlayMode/UI/TMPUtilsTests.cs
@@ -1,3 +1,44 @@
-﻿namespace Packages.BrandonUtils.Tests.EditMode.UI {
-    public class TMPUtilsTests { }
+﻿using System;
+using System.Linq;
+
+using NUnit.Framework;
+
+using Packages.BrandonUtils.Runtime.Strings;
+using Packages.BrandonUtils.Runtime.UI;
+
+using TMPro;
+
+using UnityEngine;
+
+using Object = UnityEngine.Object;
+
+namespace Packages.BrandonUtils.Tests.EditMode.UI {
+    public class TMPUtilsTests {
+        private const string CANVAS_PATH = "TestMeshProCanvas";
+
+        private string Prose = "This is the prose that I'm going to be putting into the TextMeshPro that I'm going to check for words and stuff.";
+
+        public static TMP_Text InstantiateTextMeshPro(string content) {
+            var canvasResource = Resources.Load<GameObject>(CANVAS_PATH);
+            var canvasObject   = Object.Instantiate(canvasResource);
+            var textMesh       = canvasObject.GetComponentInChildren<TMP_Text>();
+            textMesh.text = content;
+            return textMesh;
+        }
+
+        [Test]
+        public void TestFirstSubstring(
+            [Values("yolo")]
+            string strToFind,
+            [Values(10)]
+            int repetitions,
+            [Values] StringComparison stringComparison
+        ) {
+            var strContent = strToFind.Repeat(repetitions);
+            var textMesh   = InstantiateTextMeshPro(strContent);
+            var foundStr   = textMesh.FirstSubstring(strToFind, stringComparison);
+
+            Assert.That(foundStr, Is.EqualTo(textMesh.textInfo.characterInfo.ToList().GetRange(0, strToFind.Length)));
+        }
+    }
 }
