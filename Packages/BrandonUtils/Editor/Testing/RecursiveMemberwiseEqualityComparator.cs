b098d411105588deb8a904a8b86966696dbdb401
brandoncimino <brandon.cimino@gmail.com>
2021-03-27 11:18:14 -0500

    Added RecursiveMemberwiseEqualityComparator.cs

---
+++ Packages/BrandonUtils/Editor/Testing/RecursiveMemberwiseEqualityComparator.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Linq;
+
+using NUnit.Framework.Constraints;
+
+using Packages.BrandonUtils.Runtime;
+using Packages.BrandonUtils.Runtime.Exceptions;
+
+namespace Packages.BrandonUtils.Editor.Testing {
+    /// <summary>
+    /// TODO: Add tests, after I refactor the project (again)!
+    /// </summary>
+    public class RecursiveMemberwiseEqualityComparator : IEqualityComparer {
+        private const int Default_Recursion_Limit = 20;
+        private       int RecursionLimit;
+
+        public RecursiveMemberwiseEqualityComparator(int recursionLimit = Default_Recursion_Limit) {
+            RecursionLimit = recursionLimit;
+        }
+
+        public bool Equals(object x, object y) {
+            return EqualsRecursively(x, y);
+        }
+
+        private static bool EqualsRecursively(object x, object y, int recursionCount = 0) {
+            Console.WriteLine($"Comparing: [{recursionCount}]\n\t[{x.GetType().Name}] {x}\n\t[{y.GetType().Name}] {y}");
+
+            recursionCount++;
+
+            if (recursionCount > Default_Recursion_Limit) {
+                throw new BrandonException($"BRANDON OVERFLOW EXCEPTION - recursion exceeded {nameof(Default_Recursion_Limit)} {Default_Recursion_Limit}");
+            }
+
+            // Attempt to compare using the default NUnitComparer first
+            var tolerance = Tolerance.Default;
+            if (NUnitEqualityComparer.Default.AreEqual(x, y, ref tolerance)) {
+                return true;
+            }
+
+            var variables = x.GetType().GetVariables();
+            return variables.All(
+                it => EqualsRecursively(
+                    ReflectionUtils.GetVariableValue(x, it.Name),
+                    ReflectionUtils.GetVariableValue(y, it.Name),
+                    recursionCount
+                )
+            );
+        }
+
+        public int GetHashCode(object obj) {
+            return base.GetHashCode();
+        }
+    }
+}
