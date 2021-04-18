a098143b218c7acc31fbc1a1c938eba1d082da88
brandoncimino <brandon.cimino@gmail.com>
2021-04-18 00:28:19 -0500

    Added some LogUtilsTests.cs for EditMode and PlayMode

---
+++ Packages/BrandonUtils/Tests/EditMode/LogUtilsTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Packages.BrandonUtils.Runtime.Logging;
+
+namespace Packages.BrandonUtils.Tests.EditMode {
+    public class LogUtilsTests {
+        [Test]
+        public void CanLogInEditMode() {
+            LogUtils.Log("I have logged this whilst in Edit Mode");
+        }
+    }
+}
---
+++ Packages/BrandonUtils/Tests/PlayMode/LogUtilsTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+using Packages.BrandonUtils.Runtime.Logging;
+
+namespace Packages.BrandonUtils.Tests.PlayMode {
+    public class LogUtilsTests {
+        [Test]
+        public void CanLogInPlayMode() {
+            LogUtils.Log("I have logged this whilst in a Play Mode test");
+        }
+    }
+}
