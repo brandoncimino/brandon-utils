0fd48e50ccfb2462fa56a176f6ac766cb2146545
brandoncimino <brandon.cimino@gmail.com>
2021-04-23 15:11:38 -0500

    Added Get-UnityCredentials

--- Packages/BrandonUtils/BuildAutomator.ps1
+++ Packages/BrandonUtils/BuildAutomator.ps1
@@ -4,28 +4,34 @@
     .SYNOPSIS
     Butler is downloaded to & expanded in $butler_dir
  #>
-function Find-ParentFolder($Path, $ParentName){
-    if(Split-Path $Path -Leaf -eq $ParentName){
+function Find-ParentFolder($Path, $ParentName) {
+    if (Split-Path $Path -Leaf -eq $ParentName) {
         return $Path
     }
 
     return Find-ParentFolder ($Path | Split-Path -Parent) $ParentName
 }
 
-function Find-UnityPackageRoot($Path = $PWD){
+function Find-UnityPackageRoot($Path = $PWD) {
+    if (!$Path) {
+        Write-Host -ForegroundColor darkmagenta "path [$path] aint 'gun work"
+        return $null
+    }
+    Write-Host -ForegroundColor magenta "checking if $Path or its parent is a the package root"
     try {
         return Join-Path $Path ".." "package.json" -Resolve | Split-Path -Parent
     }
     catch {
-        return Find-UnityPackageRoot ($Path | Split-Path)
+        return Find-UnityPackageRoot ($Path | Split-Path -Parent)
     }
 }
 
-function Find-UnityProjectRoot($Path = $PWD){
+function Find-UnityProjectRoot($Path = $PWD) {
     $has_assets_folder = Test-Path (Join-Path $Path "Assets")
     $has_library_folder = Test-Path(Join-Path $Path "Library")
+    $has_packages_folder = Test-Path(Join-Path $Path "Packages")
 
-    if($has_assets_folder -and $has_library_folder){
+    if ($has_assets_folder -and $has_library_folder -and $has_packages_folder) {
         return $Path
     }
 
@@ -34,7 +40,6 @@ function Find-UnityProjectRoot($Path = $PWD){
 
 # Exit if there is any error in the build script
 $ErrorActionPreference = "Stop"
-$unity_project_root = Find-UnityProjectRoot
 
 #region Enums
 enum OS {
@@ -42,12 +47,6 @@ enum OS {
     Mac
     Linux
 }
-
-enum UnityBuildTarget {
-
-}
-#endregion
-
 function Get-OS() {
     if ($IsWindows) {
         return [OS]::Windows
@@ -63,11 +62,17 @@ function Get-OS() {
     }
 }
 
+enum UnityBuildTarget {
+    WebGL
+}
+#endregion
+
+
 #tag::install-butler[]
 function Install-Butler() {
     #region butler constants
     $butler_name = "butler"
-    $butler_dir = Join-Path $unity_project_root $butler_name
+    $butler_dir = Join-Path (Find-UnityProjectRoot) $butler_name
     $butler_base_url = "https://broth.itch.ovh/butler"
     $butler_release_channels = @{
         [OS]::Windows = "windows-amd64"
@@ -113,9 +118,40 @@ function Install-Butler() {
 }
 #end::install-butler[]
 
+function Get-UnityCredentials() {
+    try {
+        $creds_file_name = "unity_credentials.json"
+
+        $creds_file_path = Join-Path (Find-UnityProjectRoot) ".." $creds_file_name
+        
+        $creds_obj = Get-Content $creds_file_path | ConvertFrom-Json
+        
+        if(!$creds_obj.password -or !$creds_obj.username){
+            throw [System.MissingFieldException]::new("You must provide both a password and a username in [$creds_file_name]! Actual content: $($creds_obj | out-string)",$_.exception)
+        }
+
+        return $creds_obj
+    }
+    catch {
+        throw [System.UnauthorizedAccessException]::new("Unable to retrieve Unity credentials from [$creds_file_name], which should be located *next to the project root folder*, at [$creds_file_path]",$_.Exception)
+    }
+
+    # I'm so tired...but I just can't bring myself to delete this...because it lines up so nicely and the colors are so nice...
+    # $cred_fallbacks = [ordered]@{
+    #     package = Get-UnityCredentialsFromFile (Join-Path (Find-UnityPackageRoot) $creds_file_name)
+    #     project = Get-UnityCredentialsFromFile (Join-Path (Find-UnityProjectRoot) $creds_file_name)
+    #     sibling = Get-UnityCredentialsFromFile (Join-Path (Find-UnityProjectRoot) ".." $creds_file_name)
+    # }
+}
 
 function Build-Unity() {
-    Unity -quit -batchmode -executeMethod BuildAutomator.WebGL
+    # $unity_executable_path = "C:\Program Files\Unity\Editor\Unity.exe"
+    # "C:\Program Files\Unity\Editor\Unity.exe" -batchmode -username name@example.edu.uk -password XXXXXXXXXXXXX -serial E3-XXXX-XXXX-XXXX-XXXX-XXXX –quit
+    $creds_obj = Get-UnityCredentials
+
+    $unity_command = "'$unity_executable_path' -batchmode -quit -nographics -executeMethod '$unity_method' -username '$($creds_obj.username)' -password '$($creds_obj.password)'"
+    &$unity_command
+    # "C:\Program Files\Unity\Editor\Unity.exe" -batchmode -quit -nographics -batchmode -executeMethod BuildAutomator.WebGL
 }
 
 function Deploy-Unity() {
