c22e5f241806071b6be90e1a369b851f5f9ef992
brandoncimino <brandon.cimino@gmail.com>
2021-04-24 17:46:33 -0500

    Added Cube.cs and CubeUtils.cs

---
+++ Packages/BrandonUtils/Runtime/Spatial/Cube.cs
@@ -0,0 +1,22 @@
+﻿namespace BrandonUtils.Spatial {
+    /**
+     * Utilities for interacting with with cubes.
+     *
+     * <remarks>
+     * May someday evolve into / be combined with a class that actually represents a cube itself.
+     * </remarks>
+     */
+    public class Cube {
+        /// <summary>
+        /// Represents one <a href="https://en.wikipedia.org/wiki/Face_(geometry)">face</a> of a cube.
+        /// </summary>
+        public enum Face {
+            Forward,
+            Backward,
+            Left,
+            Right,
+            Up,
+            Down
+        }
+    }
+}
---
+++ Packages/BrandonUtils/Runtime/Spatial/CubeUtils.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+using BrandonUtils.Standalone.Enums;
+
+using UnityEngine;
+
+namespace BrandonUtils.Spatial {
+    public static class CubeUtils {
+        /// <summary>
+        /// Returns the the corresponding <see cref="Vector3.normalized"/> <see cref="Vector3"/> that <paramref name="self"/> points to,
+        /// e.g. <see cref="Cube.Face"/>.<see cref="Cube.Face.Forward"/>.<see cref="NormalizedVector"/> -> <see cref="Vector3"/>.<see cref="Vector3.forward"/>
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidEnumArgumentException">If an unknown </exception>
+        public static Vector3 NormalizedVector(this Cube.Face self) {
+            return self switch {
+                Cube.Face.Forward  => Vector3.forward,
+                Cube.Face.Backward => Vector3.back,
+                Cube.Face.Left     => Vector3.left,
+                Cube.Face.Right    => Vector3.right,
+                Cube.Face.Up       => Vector3.up,
+                Cube.Face.Down     => Vector3.down,
+                _                  => throw EnumUtils.InvalidEnumArgumentException(nameof(self), self)
+            };
+        }
+    }
+}
