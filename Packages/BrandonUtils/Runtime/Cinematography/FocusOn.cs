1711f2345157041dff5b13099745d9f65edf6220
brandoncimino <brandon.cimino@gmail.com>
2021-06-01 16:11:59 -0500

    Updated javadocs

--- Packages/BrandonUtils/Runtime/Cinematography/FocusOn.cs
+++ Packages/BrandonUtils/Runtime/Cinematography/FocusOn.cs
@@ -1,16 +1,16 @@
 ﻿namespace BrandonUtils.Cinematography {
     public enum FocusOn {
         /// <summary>
-        /// Directs the <see cref="DollyGrip"/> to look at the <see cref="DollyGrip.Subject_World"/>.
+        /// Refers directly to the <see cref="DollyCrewman.Subject"/>.
         /// </summary>
         Subject,
         /// <summary>
-        /// Directs the <see cref="DollyGrip"/> to look at the <a href="https://en.wiktionary.org/wiki/middle_distance">middle distance</a>
-        /// between their <see cref="DollyGrip.DollyMark_World"/> and the <see cref="DollyGrip.Subject_World"/>
+        /// Refers to the <a href="https://en.wiktionary.org/wiki/middle_distance">middle distance</a>, which is usually
+        /// between the <see cref="DollyCrewman.Origin"/> and <see cref="DollyCrewman.Subject"/>.
         /// </summary>
         MiddleDistance,
         /// <summary>
-        /// The <see cref="DollyGrip"/> won't actively look anywhere, essentially bypassing <see cref="DollyGrip.PointCamera"/>.
+        /// The <see cref="DollyCrewman"/> won't actively look anywhere.
         /// </summary>
         Nothing
     }
