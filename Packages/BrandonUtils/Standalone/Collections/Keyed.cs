1e5025e226b65e8e7a4460d3b4864026e357c738
brandoncimino <brandon.cimino@gmail.com>
2021-06-02 00:32:56 -0500

    Added Keyed.cs, a fancy way to interact with IPrimaryKeyed

---
+++ Packages/BrandonUtils/Standalone/Collections/Keyed.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BrandonUtils.Standalone.Collections {
+    /// <summary>
+    /// Essentially creates an <see cref="IPrimaryKeyed{T}"/> out of <see cref="Value"/>, where the <see cref="IPrimaryKeyed{T}.PrimaryKey"/> is determined by <see cref="Func{T,TResult}.Invoke"/>-ing <see cref="KeySelector"/>.
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="TKey"></typeparam>
+    public class Keyed<TValue, TKey> : IPrimaryKeyed<TKey> {
+        public TValue             Value;
+        public Func<TValue, TKey> KeySelector;
+        public TKey               PrimaryKey => KeySelector.Invoke(Value);
+
+        public Keyed(TValue value, Func<TValue, TKey> keySelector) {
+            this.Value       = value;
+            this.KeySelector = keySelector;
+        }
+
+        public static Keyed<TValue, TKey> Of(TValue value, Func<TValue, TKey> keySelector) {
+            return new Keyed<TValue, TKey>(value, keySelector);
+        }
+    }
+}
