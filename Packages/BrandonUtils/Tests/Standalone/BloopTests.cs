90a95c989ab21bac2f21a5793b473da3c4a5f506
brandoncimino <brandon.cimino@gmail.com>
2021-06-05 23:17:27 -0500

    Added BloopTests.cs! Wooh!

---
+++ Packages/BrandonUtils/Tests/Standalone/BloopTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using BrandonUtils.Standalone;
+using BrandonUtils.Standalone.Collections;
+
+using NUnit.Framework;
+
+namespace BrandonUtils.Tests.Standalone {
+    /// <summary>
+    /// Tests for <see cref="Bloop"/>s
+    /// </summary>
+    public class BloopTests {
+        [Test]
+        [TestCase(5)]
+        public void RepeatRandom(int numberOfPicks) {
+            var random     = new Random();
+            var picks      = numberOfPicks.Repeat(() => random.Next());
+            var pickGroups = picks.Group();
+            Assert.That(pickGroups, Has.Count.EqualTo(numberOfPicks));
+        }
+    }
+}
