16c9772b13415ceae191531296a87e4b7837655d
brandoncimino <brandon.cimino@gmail.com>
2021-06-09 23:59:27 -0500

    I dunno how this got here

--- Packages/BrandonUtils/Runtime/InRange.cs
+++ Packages/BrandonUtils/Runtime/InRange.cs
@@ -3,6 +3,9 @@
 using UnityEngine;
 
 namespace BrandonUtils.Standalone.Randomization {
+    /// <summary>
+    /// TODO: How did this get here...?
+    /// </summary>
     public class InRange : Randomized<float> {
         public Vector2 Range;
 
