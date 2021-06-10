0847340d5c8f22f9c37b6e9ebc60471d86649545
brandoncimino <brandon.cimino@gmail.com>
2021-06-09 23:45:33 -0500

    Added SugarMath.cs, which is in the cutely named Mathb.meta folder

---
+++ Packages/BrandonUtils/Runtime/Mathb/SugarMath.cs
@@ -0,0 +1,27 @@
+﻿using BrandonUtils.Vectors;
+
+using UnityEngine;
+
+namespace BrandonUtils.Mathb {
+    public static class SugarMath {
+        public static float Clamp(this float f, float min, float max) {
+            return Mathf.Clamp(f, min, max);
+        }
+
+        public static float Clamp(this float f, Vector2 range) {
+            return Mathf.Clamp(f, range.Min(), range.Max());
+        }
+
+        public static float Clamp01(this float f) {
+            return Mathf.Clamp01(f);
+        }
+
+        public static int Clamp(this int i, int min, int max) {
+            return Mathf.Clamp(i, min, max);
+        }
+
+        public static int Clamp(this int i, Vector2Int range) {
+            return Mathf.Clamp(i, range.Min(), range.Max());
+        }
+    }
+}
