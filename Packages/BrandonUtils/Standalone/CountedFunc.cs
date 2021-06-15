368f4751d7cb858c917a1cb7ed64fcf2e2405d8e
brandoncimino <brandon.cimino@gmail.com>
2021-06-15 00:50:13 -0500

    Added CountedFunc.cs

---
+++ Packages/BrandonUtils/Standalone/CountedFunc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BrandonUtils.Standalone {
+    /// <summary>
+    /// A wrapper for a <see cref="Func{TResult}"/> that counts the number of times it was <see cref="Func{TResult}.Invoke"/>d
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    public class CountedFunc<TResult> {
+        private readonly Func<TResult> Func;
+        public           int           InvocationCount { get; private set; }
+
+        public CountedFunc(Func<TResult> func) {
+            Func = func;
+        }
+
+        public TResult Invoke() {
+            InvocationCount += 1;
+            return Func.Invoke();
+        }
+
+        public static implicit operator TResult(CountedFunc<TResult> self) {
+            return self.Invoke();
+        }
+
+        public static implicit operator CountedFunc<TResult>(Func<TResult> func) {
+            return new CountedFunc<TResult>(func);
+        }
+    }
+}
