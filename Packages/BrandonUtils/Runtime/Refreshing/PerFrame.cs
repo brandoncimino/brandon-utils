4f458515c10ebb34916bdc2d324172eee5b95af4
brandoncimino <brandon.cimino@gmail.com>
2021-06-17 23:41:47 -0500

    Added some convenient stuff for Refreshing.PerFrame

---
+++ Packages/BrandonUtils/Runtime/Refreshing/PerFrame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using BrandonUtils.Standalone.Refreshing;
+
+using UnityEngine;
+
+namespace BrandonUtils.Refreshing {
+    public class PerFrame<TValue> : Refreshing<TValue, int> where TValue : IEquatable<TValue> {
+        public PerFrame(Func<TValue> valueSupplier) : base(
+            valueSupplier,
+            () => Time.frameCount,
+            (previous, current) => current != previous
+        ) { }
+    }
+}
---
+++ Packages/BrandonUtils/Runtime/Refreshing/Refreshing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BrandonUtils.Refreshing {
+    public static class Refreshing {
+        /// <summary>
+        /// Constructs a new <see cref="PerFrame{T}"/>.
+        /// </summary>
+        /// <param name="valueSupplier"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static PerFrame<T> PerFrame<T>(Func<T> valueSupplier) where T : IEquatable<T> {
+            return new PerFrame<T>(valueSupplier);
+        }
+    }
+}
