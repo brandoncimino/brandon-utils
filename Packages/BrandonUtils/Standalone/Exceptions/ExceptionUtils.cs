8a7e9a20c96fc9e6f63335829763c823e38c292d
brandoncimino <brandon.cimino@gmail.com>
2021-06-26 22:11:30 -0500

    Added ExceptionUtils.cs

---
+++ Packages/BrandonUtils/Standalone/Exceptions/ExceptionUtils.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Reflection;
+
+namespace BrandonUtils.Standalone.Exceptions {
+    public static class ExceptionUtils {
+        private static readonly MethodInfo ModifyMessageMethod = typeof(ExceptionUtils).GetMethod(
+            nameof(ModifyMessage_Internal),
+            BindingFlags.Default |
+            BindingFlags.Static |
+            BindingFlags.NonPublic
+        );
+
+        private static T ModifyMessage_Internal<T>(T exception, string newMessage) where T : Exception {
+            try {
+                return exception.InnerException == null ? ReflectionUtils.Construct<T>($"{newMessage}\n{exception.Message}") : ReflectionUtils.Construct<T>($"{newMessage}\n{exception.Message}", exception.InnerException);
+            }
+            catch (Exception e) {
+                throw new BrandonException($"Couldn't modify the exception message!\n\tOriginal message: {exception.Message}\n\tNew message: {newMessage}", e);
+            }
+        }
+
+        /// <summary>
+        /// Returns a new copy of <paramref name="exception"/> with the <see cref="Exception.Message"/> changed to <paramref name="newMessage"/>.
+        /// </summary>
+        /// <remarks>
+        /// The new exception <i>will</i> maintain the actual type of the original <paramref name="exception"/>.
+        /// </remarks>
+        /// <param name="exception">the original <see cref="Exception"/></param>
+        /// <param name="newMessage">the new <see cref="Exception.Message"/></param>
+        /// <returns>a new copy of <paramref name="exception"/> with the <see cref="Exception.Message"/> changed to <paramref name="newMessage"/></returns>
+        public static Exception ModifyMessage(this Exception exception, string newMessage) {
+            var genericModifyMessage = ModifyMessageMethod.MakeGenericMethod(exception.GetType());
+            return genericModifyMessage.Invoke(null, new object[] {exception, newMessage}) as Exception;
+        }
+
+        /**
+         * <inheritdoc cref="ModifyMessage"/>
+         */
+        public static T ModifyMessage<T>(this T exception, string newMessage) where T : Exception {
+            return ModifyMessage_Internal(exception, newMessage);
+        }
+
+        /// <summary>
+        /// Returns a new copy of <paramref name="exception"/> with the <see cref="Exception.Message"/> prepended with <paramref name="additionalMessage"/>.
+        /// </summary>
+        /// <remarks>
+        /// <inheritdoc cref="ModifyMessage"/>
+        /// </remarks>
+        /// <param name="exception"><inheritdoc cref="ModifyMessage"/></param>
+        /// <param name="additionalMessage">the string to prepend the original's <see cref="Exception.Message"/></param>
+        /// <returns></returns>
+        public static Exception PrependMessage(this Exception exception, string additionalMessage) {
+            return ModifyMessage(exception, $"{additionalMessage}\n{exception.Message}");
+        }
+
+        /**
+         * <inheritdoc cref="PrependMessage"/>
+         */
+        public static T PrependMessage<T>(this T exception, string additionalMessage) where T : Exception {
+            return ModifyMessage_Internal(exception, $"{additionalMessage}\n{exception.Message}");
+        }
+    }
+}
---
+++ Packages/BrandonUtils/Tests/Standalone/ExceptionUtilsTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using BrandonUtils.Standalone.Exceptions;
+using BrandonUtils.Testing;
+
+using NUnit.Framework;
+
+namespace BrandonUtils.Tests.Standalone {
+    public class ExceptionUtilsTests {
+        private const string Prepended = "PREPENDED";
+        private const string Original  = "ORIGINAL";
+
+        [Test]
+        public void PrependMessage_KnownType() {
+            try {
+                throw new NullReferenceException(Original);
+            }
+            catch (NullReferenceException e) {
+                var e2 = e.PrependMessage(Prepended);
+                AssertAll.Of(
+                    e2,
+                    Is.Not.Null,
+                    Has.Property(nameof(e2.Message)).StartsWith(Prepended),
+                    Has.Property(nameof(e2.Message)).EndsWith(Original),
+                    Is.TypeOf<NullReferenceException>()
+                );
+            }
+        }
+
+        [Test]
+        public void PrependMessage_UnknownType() {
+            try {
+                throw new NullReferenceException(Original);
+            }
+            catch (Exception e) {
+                var e2 = e.PrependMessage(Prepended);
+
+                AssertAll.Of(
+                    e2,
+                    Is.Not.Null,
+                    Is.TypeOf<NullReferenceException>(),
+                    Is.InstanceOf<NullReferenceException>(),
+                    Is.AssignableTo<NullReferenceException>(),
+                    Has.Property(nameof(e.Message)).StartsWith(Prepended),
+                    Has.Property(nameof(e.Message)).EndsWith(Original)
+                );
+            }
+        }
+    }
+}
