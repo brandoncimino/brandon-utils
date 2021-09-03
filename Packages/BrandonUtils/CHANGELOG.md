08540672e94131a6f9694449a65decf6f7d02eb9
brandoncimino <brandon.cimino@gmail.com>
2021-09-03 10:18:42 -0500

    Added List.Of()

--- Packages/BrandonUtils/CHANGELOG.md
+++ Packages/BrandonUtils/CHANGELOG.md
@@ -5,7 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres
 to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [2.17.2] - 9/2/2021
+## [2.17.3] - 9/3/2021
+
+### Added
+
+- `List.Of()`
+
+## [2.17.2] - 9/3/2021
 
 ### Added
 
---
+++ Packages/BrandonUtils/Standalone/Collections/List.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace BrandonUtils.Standalone.Collections {
+    /// <summary>
+    /// A class created exclusively to hold the <see cref="Of{T}"/> method, which mimics Java's <a href="https://docs.oracle.com/javase/9/docs/api/java/util/List.html#of-E...-">List.of()</a>.
+    /// </summary>
+    /// <remarks>
+    /// If this causes import conflicts and stuff when typing, the class can be renamed to <c>"Lists"</c>.
+    /// </remarks>
+    public static class List {
+        /// <summary>
+        /// Mimics Java's <a href="https://docs.oracle.com/javase/9/docs/api/java/util/List.html#of-E...-">List.of()</a>, because I keep typing that constantly.
+        /// </summary>
+        public static List<T> Of<T>(params T[] entries) {
+            return new List<T>(entries);
+        }
+    }
+}
