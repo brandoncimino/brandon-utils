34d70a5f81184b5c6fa0ad655e8ea04aec49b556
brandoncimino <brandon.cimino@gmail.com>
2021-09-06 16:21:38 -0500

    Added attributes to track backing fields: - `BackedByAttribute` - `BackingFieldForAttribute`

---
+++ Packages/BrandonUtils/Standalone/Attributes/BackedByAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using JetBrains.Annotations;
+
+namespace BrandonUtils.Standalone.Attributes {
+    [PublicAPI]
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class BackedByAttribute : BrandonAttribute {
+        [NotNull] public string BackingFieldName { get; }
+
+        public BackedByAttribute([NotNull] string backingFieldName) {
+            BackingFieldName = backingFieldName;
+        }
+    }
+}
---
+++ Packages/BrandonUtils/Standalone/Attributes/BackingFieldFor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+using JetBrains.Annotations;
+
+namespace BrandonUtils.Standalone.Attributes {
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true, Inherited = true)]
+    [PublicAPI]
+    public class BackingFieldForAttribute : BrandonAttribute {
+        [NotNull] public string BackedPropertyName { get; }
+
+        [CanBeNull]
+        public PropertyInfo BackedProperty { get; }
+
+        public BackingFieldForAttribute([NotNull] string backedPropertyName) {
+            BackedPropertyName = backedPropertyName;
+        }
+    }
+}
