a878e120451bb802d202ec544135306ecef188af
brandoncimino <brandon.cimino@gmail.com>
2021-09-09 19:07:45 -0500

    Some bonus PrimaryKeyedListTests.cs

--- Packages/BrandonUtils/Tests/Standalone/Collections/PrimaryKeyedListTests.cs
+++ Packages/BrandonUtils/Tests/Standalone/Collections/PrimaryKeyedListTests.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 
 using BrandonUtils.Standalone.Collections;
+using BrandonUtils.Testing;
 
 using Newtonsoft.Json;
 
 using NUnit.Framework;
+
+using Is = NUnit.Framework.Is;
 
 namespace BrandonUtils.Tests.Standalone.Collections {
     public class PrimaryKeyedListTests {
@@ -40,7 +43,12 @@ namespace BrandonUtils.Tests.Standalone.Collections {
             public AlsoHasInterface(DayOfWeek dayOfWeek) : base(dayOfWeek) { }
         }
 
-        private static KeyedList<DayOfWeek, HasInterface> NewListOfDays() => new PrimaryKeyedList<DayOfWeek, HasInterface> {
+        private struct StructWithInterface : IPrimaryKeyed<DayOfWeek> {
+            public DayOfWeek DayOfWeek;
+            public DayOfWeek PrimaryKey => DayOfWeek;
+        }
+
+        private static PrimaryKeyedList<DayOfWeek, HasInterface> NewListOfDays() => new PrimaryKeyedList<DayOfWeek, HasInterface> {
             new HasInterface(DayOfWeek.Monday),
             new HasInterface(DayOfWeek.Tuesday),
             new HasInterface(DayOfWeek.Wednesday),
@@ -65,7 +73,70 @@ namespace BrandonUtils.Tests.Standalone.Collections {
             }
         }
 
-        private static KeyedList<string, StringKey> StringKeyedList = new PrimaryKeyedList<string, StringKey>() { new StringKey("one", 1), new StringKey("two", 2), new StringKey("three", 3) };
+        private static KeyedList<string, StringKey> StringKeyedList = new PrimaryKeyedList<string, StringKey>() {
+            new StringKey("one", 1), new StringKey("two", 2), new StringKey("three", 3)
+        };
+
+        private class HoldsPrimaryKeyedList : IEquatable<HoldsPrimaryKeyedList>, IEqualityComparer<HoldsPrimaryKeyedList> {
+            public PrimaryKeyedList<DayOfWeek, StructWithInterface> MyKeyedList;
+
+            public bool Equals(HoldsPrimaryKeyedList other) {
+                if (ReferenceEquals(null, other)) {
+                    return false;
+                }
+
+                if (ReferenceEquals(this, other)) {
+                    return true;
+                }
+
+                // return Equals(MyKeyedList, other.MyKeyedList);
+                return MyKeyedList.SequenceEqual(other.MyKeyedList);
+            }
+
+            public override bool Equals(object obj) {
+                if (ReferenceEquals(null, obj)) {
+                    return false;
+                }
+
+                if (ReferenceEquals(this, obj)) {
+                    return true;
+                }
+
+                if (obj.GetType() != this.GetType()) {
+                    return false;
+                }
+
+                return Equals((HoldsPrimaryKeyedList)obj);
+            }
+
+            public override int GetHashCode() {
+                return (MyKeyedList != null ? MyKeyedList.GetHashCode() : 0);
+            }
+
+            public bool Equals(HoldsPrimaryKeyedList x, HoldsPrimaryKeyedList y) {
+                if (ReferenceEquals(x, y)) {
+                    return true;
+                }
+
+                if (ReferenceEquals(x, null)) {
+                    return false;
+                }
+
+                if (ReferenceEquals(y, null)) {
+                    return false;
+                }
+
+                if (x.GetType() != y.GetType()) {
+                    return false;
+                }
+
+                return Equals(x.MyKeyedList, y.MyKeyedList);
+            }
+
+            public int GetHashCode(HoldsPrimaryKeyedList obj) {
+                return (obj.MyKeyedList != null ? obj.MyKeyedList.GetHashCode() : 0);
+            }
+        }
 
         [Test]
         public void KeyedListSerializes() {
@@ -124,7 +195,9 @@ namespace BrandonUtils.Tests.Standalone.Collections {
 
         [Test]
         public void AddViaKey() {
-            var kl = new PrimaryKeyedList<DayOfWeek, HasInterface>() { new HasInterface(DayOfWeek.Monday) };
+            var kl = new PrimaryKeyedList<DayOfWeek, HasInterface>() {
+                new HasInterface(DayOfWeek.Monday)
+            };
 
             int initialCount = kl.Count;
 
@@ -142,36 +215,48 @@ namespace BrandonUtils.Tests.Standalone.Collections {
 
         [Test]
         public void ErrorCreatingFromDuplicateList() {
-            var ls = new List<HasInterface> { new HasInterface(DayOfWeek.Monday), new HasInterface(DayOfWeek.Tuesday), new HasInterface(DayOfWeek.Tuesday), new HasInterface(DayOfWeek.Tuesday) };
+            var ls = new List<HasInterface> {
+                new HasInterface(DayOfWeek.Monday), new HasInterface(DayOfWeek.Tuesday), new HasInterface(DayOfWeek.Tuesday), new HasInterface(DayOfWeek.Tuesday)
+            };
             Assert.Throws<ArgumentException>(() => new PrimaryKeyedList<DayOfWeek, HasInterface>(ls));
         }
 
         [Test]
         public void CanCreateFromValidList() {
-            var ls = new List<HasInterface> { new HasInterface(DayOfWeek.Monday), new HasInterface(DayOfWeek.Tuesday), new HasInterface(DayOfWeek.Wednesday) };
+            var ls = new List<HasInterface> {
+                new HasInterface(DayOfWeek.Monday), new HasInterface(DayOfWeek.Tuesday), new HasInterface(DayOfWeek.Wednesday)
+            };
 
-            var kl = new PrimaryKeyedList<DayOfWeek, HasInterface> { new HasInterface(DayOfWeek.Monday), new HasInterface(DayOfWeek.Tuesday), new HasInterface(DayOfWeek.Wednesday) };
+            var kl = new PrimaryKeyedList<DayOfWeek, HasInterface> {
+                new HasInterface(DayOfWeek.Monday), new HasInterface(DayOfWeek.Tuesday), new HasInterface(DayOfWeek.Wednesday)
+            };
 
             Assert.That(new PrimaryKeyedList<DayOfWeek, HasInterface>(ls), Is.EqualTo(kl));
         }
 
         [Test]
         public void ErrorOnMissingKey() {
-            var kl = new PrimaryKeyedList<DayOfWeek, HasInterface>() { new HasInterface(DayOfWeek.Tuesday) };
+            var kl = new PrimaryKeyedList<DayOfWeek, HasInterface>() {
+                new HasInterface(DayOfWeek.Tuesday)
+            };
 
             Assert.Throws<KeyNotFoundException>(() => kl[DayOfWeek.Monday].Info = "yolo");
         }
 
         [Test]
         public void ErrorOnRemovingMissingKey() {
-            var kl = new PrimaryKeyedList<DayOfWeek, HasInterface>() { new HasInterface(DayOfWeek.Tuesday) };
+            var kl = new PrimaryKeyedList<DayOfWeek, HasInterface>() {
+                new HasInterface(DayOfWeek.Tuesday)
+            };
 
             Assert.False(kl.Remove(DayOfWeek.Monday));
         }
 
         [Test]
         public void TestRemove() {
-            var kl = new PrimaryKeyedList<DayOfWeek, HasInterface>() { new HasInterface(DayOfWeek.Monday), new HasInterface(DayOfWeek.Tuesday) };
+            var kl = new PrimaryKeyedList<DayOfWeek, HasInterface>() {
+                new HasInterface(DayOfWeek.Monday), new HasInterface(DayOfWeek.Tuesday)
+            };
 
             var initialLength = kl.Count;
 
@@ -183,7 +268,9 @@ namespace BrandonUtils.Tests.Standalone.Collections {
 
         [Test]
         public void TestJsonPopulateWithSameData() {
-            var kl = new PrimaryKeyedList<DayOfWeek, HasInterface> { new HasInterface(DayOfWeek.Monday), new HasInterface(DayOfWeek.Tuesday) };
+            var kl = new PrimaryKeyedList<DayOfWeek, HasInterface> {
+                new HasInterface(DayOfWeek.Monday), new HasInterface(DayOfWeek.Tuesday)
+            };
 
             var json = JsonConvert.SerializeObject(kl);
             JsonConvert.PopulateObject(json, kl);
@@ -191,7 +278,9 @@ namespace BrandonUtils.Tests.Standalone.Collections {
 
         [Test]
         public void TestJsonPopulateEmpty() {
-            var kl = new PrimaryKeyedList<DayOfWeek, HasInterface> { new HasInterface(DayOfWeek.Monday), new HasInterface(DayOfWeek.Tuesday) };
+            var kl = new PrimaryKeyedList<DayOfWeek, HasInterface> {
+                new HasInterface(DayOfWeek.Monday), new HasInterface(DayOfWeek.Tuesday)
+            };
 
             var json = JsonConvert.SerializeObject(kl);
 
@@ -202,6 +291,29 @@ namespace BrandonUtils.Tests.Standalone.Collections {
             JsonConvert.PopulateObject(json, kl2);
 
             Assert.That(kl2, Is.EqualTo(kl));
+        }
+
+        [Test]
+        public void SerializationOfObjectHoldingAKeyedList() {
+            var holdingKeyedList = new HoldsPrimaryKeyedList() {
+                MyKeyedList = new PrimaryKeyedList<DayOfWeek, StructWithInterface>() {
+                    new StructWithInterface() {
+                        DayOfWeek = DayOfWeek.Monday
+                    },
+                    new StructWithInterface() {
+                        DayOfWeek = DayOfWeek.Tuesday
+                    }
+                }
+            };
+
+            var toJson = JsonConvert.SerializeObject(holdingKeyedList);
+
+            var fromJson = JsonConvert.DeserializeObject<HoldsPrimaryKeyedList>(toJson);
+
+            AssertAll.Of(
+                () => Assert.That(holdingKeyedList.MyKeyedList, Is.EqualTo(fromJson.MyKeyedList)),
+                () => Assert.That(holdingKeyedList,             Is.EqualTo(fromJson))
+            );
         }
     }
 }
