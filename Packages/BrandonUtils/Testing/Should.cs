59cceb0de2efa5f0a6304a9edd4d6c0bd4c10997
brandoncimino <brandon.cimino@gmail.com>
2021-09-20 00:01:38 -0500

    Added `Should` enum to be shared across tests (instead of them each having their own)

---
+++ Packages/BrandonUtils/Testing/Should.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using BrandonUtils.Standalone.Enums;
+
+using NUnit.Framework.Constraints;
+
+namespace BrandonUtils.Testing {
+    public enum Should {
+        Pass,
+        Fail
+    }
+
+    [SuppressMessage("ReSharper", "AccessToStaticMemberViaDerivedType")]
+    public static class ShouldExtensions {
+        public static bool ToBool(this Should should) {
+            return should switch {
+                Should.Pass => true,
+                Should.Fail => false,
+                _           => throw BEnum.InvalidEnumArgumentException(nameof(should), should)
+            };
+        }
+
+        public static Constraint ToCondition(this Should should) {
+            return should switch {
+                Should.Pass => Is.True,
+                Should.Fail => Is.False,
+                _           => throw BEnum.InvalidEnumArgumentException(nameof(should), should)
+            };
+        }
+    }
+}
