01bd0ae8d9944f09976ffe19957a80a215152de1
brandoncimino <brandon.cimino@gmail.com>
2021-10-10 22:16:04 -0500

    Added `DictionaryExtensions.cs` and `GetOrDefault()`

---
+++ Packages/BrandonUtils/Standalone/Collections/DictionaryExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
+namespace BrandonUtils.Standalone.Collections {
+    public static class DictionaryExtensions {
+        [CanBeNull]
+        public static TValue GetOrDefault<TKey, TValue>(
+            [NotNull] this IDictionary<TKey, TValue> dic,
+            [NotNull]      TKey                      key,
+            [CanBeNull]    TValue                    fallback = default
+        ) {
+            if (dic == null) {
+                throw new ArgumentNullException(nameof(dic));
+            }
+
+            if (key == null) {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            return dic.ContainsKey(key) ? dic[key] : fallback;
+        }
+
+        [CanBeNull]
+        public static TValue GetOrDefault<TKey, TValue>(
+            [NotNull] this IDictionary<TKey, TValue> dic,
+            [NotNull]      TKey                      key,
+            [NotNull]      Func<TValue>              fallbackSupplier
+        ) {
+            if (dic == null) {
+                throw new ArgumentNullException(nameof(dic));
+            }
+
+            if (key == null) {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (fallbackSupplier == null) {
+                throw new ArgumentNullException(nameof(fallbackSupplier));
+            }
+
+            return dic.ContainsKey(key) ? dic[key] : fallbackSupplier.Invoke();
+        }
+    }
+}
