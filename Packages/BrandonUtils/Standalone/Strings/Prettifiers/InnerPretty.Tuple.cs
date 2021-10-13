8be12d85118e7556c4797c0ab98791f189ef93f6
brandoncimino <brandon.cimino@gmail.com>
2021-10-12 23:08:37 -0500

    Limited Tuple prettifiers to T7, T8+

--- Packages/BrandonUtils/Standalone/Strings/Prettifiers/InnerPretty.Tuple.cs
+++ Packages/BrandonUtils/Standalone/Strings/Prettifiers/InnerPretty.Tuple.cs
@@ -37,33 +37,6 @@ namespace BrandonUtils.Standalone.Strings.Prettifiers {
         public static string Tuple7<T, T2, T3, T4, T5, T6, T7>((T, T2, T3, T4, T5, T6, T7) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
 
         [NotNull, Pure]
-        public static string Tuple8<T, T2, T3, T4, T5, T6, T7, T8>((T, T2, T3, T4, T5, T6, T7, T8) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
-
-        [NotNull, Pure]
-        public static string Tuple9<T, T2, T3, T4, T5, T6, T7, T8, T9>((T, T2, T3, T4, T5, T6, T7, T8, T9) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
-
-        [NotNull, Pure]
-        public static string Tuple10<T, T2, T3, T4, T5, T6, T7, T8, T9, T10>((T, T2, T3, T4, T5, T6, T7, T8, T9, T10) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
-
-        [NotNull, Pure]
-        public static string Tuple11<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>((T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
-
-        [NotNull, Pure]
-        public static string Tuple12<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>((T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
-
-        [NotNull, Pure]
-        public static string Tuple13<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>((T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
-
-        [NotNull, Pure]
-        public static string Tuple14<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>((T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
-
-        [NotNull, Pure]
-        public static string Tuple15<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>((T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
-
-        [NotNull, Pure]
-        public static string Tuple16<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>((T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
-
-        [NotNull, Pure]
-        public static string Tuple17<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17>((T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
+        public static string Tuple8Plus<T, T2, T3, T4, T5, T6, T7, T8>((T, T2, T3, T4, T5, T6, T7, T8) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
     }
 }
