71231536382d1be6a73e2df9b6ee16abd439871e
brandoncimino <brandon.cimino@gmail.com>
2021-10-14 17:46:44 -0500

    Added Nullable extensions to convert from `T?` -> `IEnumerable<T>` and `Optional<T>`

---
+++ Packages/BrandonUtils/Standalone/Optional/NullableExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+using JetBrains.Annotations;
+
+using Pure = System.Diagnostics.Contracts.PureAttribute;
+
+namespace BrandonUtils.Standalone.Optional {
+    [PublicAPI]
+    public static class NullableExtensions {
+        [Pure]
+        [NotNull]
+        public static IEnumerable<T> AsEnumerable<T>(this T? nullableValue) where T : struct {
+            return nullableValue.HasValue ? Enumerable.Repeat(nullableValue.Value, 1) : Enumerable.Empty<T>();
+        }
+
+        [Pure]
+        public static Optional<T> ToOptional<T>(this T? nullableValue) where T : struct {
+            return nullableValue.HasValue ? Optional.Of(nullableValue.Value) : default;
+        }
+
+        [Pure]
+        public static T? ToNullable<T>([NotNull] this IOptional<T> optional) where T : struct {
+            return optional.OrElse(default);
+        }
+
+        [Pure]
+        public static T? ToNullable<T>([NotNull] this IOptional<T?> optional) where T : struct {
+            return optional.OrElse(default);
+        }
+    }
+}
