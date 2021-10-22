9d0f9cf6ca3643da266290a0d0166c39e51e9bbc
brandoncimino <brandon.cimino@gmail.com>
2021-10-22 17:05:02 -0500

    Added `ValidationExtensions.cs`

---
+++ Packages/BrandonUtils/Standalone/ValidationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BrandonUtils.Standalone.Strings;
+
+using JetBrains.Annotations;
+
+namespace BrandonUtils.Standalone {
+    [PublicAPI]
+    public static class ValidationExtensions {
+        [NotNull]
+        [ContractAnnotation("null => stop")]
+        public static T MustNotBeNull<T>([CanBeNull] this T obj) {
+            return obj ?? throw new ArgumentNullException($"Validated Parameter of type {typeof(T).Prettify()}");
+        }
+    }
+}
