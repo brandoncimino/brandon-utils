b15761fcaf42ec7fdb7ab2103fa9988430103ff7
brandoncimino <brandon.cimino@gmail.com>
2021-10-22 16:58:59 -0500

    Prevented ambiguity between `ToArray()` methods for tuples by renaming some to `ToObjArray` - Added `Sort` tuple extensions

--- Packages/BrandonUtils/Standalone/Collections/TupleExtensions.cs
+++ Packages/BrandonUtils/Standalone/Collections/TupleExtensions.cs
@@ -1,8 +1,13 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using System.Linq;
+
+using JetBrains.Annotations;
 
 namespace BrandonUtils.Standalone.Collections {
     [PublicAPI]
     public static class TupleExtensions {
+        #region ToArray
+
         [Pure, NotNull, ItemCanBeNull]
         public static T[] ToArray<T>(this (T, T) tuple) => new[] { tuple.Item1, tuple.Item2 };
 
@@ -53,51 +58,93 @@ namespace BrandonUtils.Standalone.Collections {
 
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2>(this (T, T2) tuple) => new object[] { tuple.Item1, tuple.Item2 };
+        public static object[] ToObjArray<T, T2>(this (T, T2) tuple) => new object[] { tuple.Item1, tuple.Item2 };
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2, T3>(this (T, T2, T3) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3 };
+        public static object[] ToObjArray<T, T2, T3>(this (T, T2, T3) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3 };
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2, T3, T4>(this (T, T2, T3, T4) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4 };
+        public static object[] ToObjArray<T, T2, T3, T4>(this (T, T2, T3, T4) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4 };
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2, T3, T4, T5>(this (T, T2, T3, T4, T5) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5 };
+        public static object[] ToObjArray<T, T2, T3, T4, T5>(this (T, T2, T3, T4, T5) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5 };
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2, T3, T4, T5, T6>(this (T, T2, T3, T4, T5, T6) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6 };
+        public static object[] ToObjArray<T, T2, T3, T4, T5, T6>(this (T, T2, T3, T4, T5, T6) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6 };
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2, T3, T4, T5, T6, T7>(this (T, T2, T3, T4, T5, T6, T7) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7 };
+        public static object[] ToObjArray<T, T2, T3, T4, T5, T6, T7>(this (T, T2, T3, T4, T5, T6, T7) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7 };
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2, T3, T4, T5, T6, T7, T8>(this (T, T2, T3, T4, T5, T6, T7, T8) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8 };
+        public static object[] ToObjArray<T, T2, T3, T4, T5, T6, T7, T8>(this (T, T2, T3, T4, T5, T6, T7, T8) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8 };
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2, T3, T4, T5, T6, T7, T8, T9>(this (T, T2, T3, T4, T5, T6, T7, T8, T9) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9 };
+        public static object[] ToObjArray<T, T2, T3, T4, T5, T6, T7, T8, T9>(this (T, T2, T3, T4, T5, T6, T7, T8, T9) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9 };
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10 };
+        public static object[] ToObjArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10 };
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10, tuple.Item11 };
+        public static object[] ToObjArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10, tuple.Item11 };
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10, tuple.Item11, tuple.Item12 };
+        public static object[] ToObjArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10, tuple.Item11, tuple.Item12 };
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10, tuple.Item11, tuple.Item12, tuple.Item13 };
+        public static object[] ToObjArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10, tuple.Item11, tuple.Item12, tuple.Item13 };
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10, tuple.Item11, tuple.Item12, tuple.Item13, tuple.Item14 };
+        public static object[] ToObjArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10, tuple.Item11, tuple.Item12, tuple.Item13, tuple.Item14 };
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10, tuple.Item11, tuple.Item12, tuple.Item13, tuple.Item14, tuple.Item15 };
+        public static object[] ToObjArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10, tuple.Item11, tuple.Item12, tuple.Item13, tuple.Item14, tuple.Item15 };
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10, tuple.Item11, tuple.Item12, tuple.Item13, tuple.Item14, tuple.Item15, tuple.Item16 };
+        public static object[] ToObjArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10, tuple.Item11, tuple.Item12, tuple.Item13, tuple.Item14, tuple.Item15, tuple.Item16 };
 
         [Pure, NotNull, ItemCanBeNull]
-        public static object[] ToArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10, tuple.Item11, tuple.Item12, tuple.Item13, tuple.Item14, tuple.Item15, tuple.Item16, tuple.Item17 };
+        public static object[] ToObjArray<T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17>(this (T, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17) tuple) => new object[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10, tuple.Item11, tuple.Item12, tuple.Item13, tuple.Item14, tuple.Item15, tuple.Item16, tuple.Item17 };
+
+        #endregion
+
+        #region Sort
+
+        public static (T min, T max) Sort<T>(this (T a, T b) tuple2) where T : IComparable<T> {
+            var ls = tuple2.ToArray().ToList();
+            ls.Sort();
+            return (ls[0], ls[1]);
+        }
+
+        public static (T, T, T) Sort<T>(this (T, T, T) tuple3) where T : IComparable<T> {
+            var ls = tuple3.ToArray().ToList();
+            ls.Sort();
+            return (ls[0], ls[1], ls[2]);
+        }
+
+        public static (T, T, T, T) Sort<T>(this (T, T, T, T) tuple4) where T : IComparable<T> {
+            var ls = tuple4.ToArray().ToList();
+            ls.Sort();
+            return (ls[0], ls[1], ls[2], ls[3]);
+        }
+
+        public static (T, T, T, T, T) Sort<T>(this (T, T, T, T, T) tuple5) where T : IComparable<T> {
+            var ls = tuple5.ToArray().ToList();
+            ls.Sort();
+            return (ls[0], ls[1], ls[2], ls[3], ls[4]);
+        }
+
+        public static (T, T, T, T, T, T) Sort<T>(this (T, T, T, T, T, T) tuple6) where T : IComparable<T> {
+            var ls = tuple6.ToArray().ToList();
+            ls.Sort();
+            return (ls[0], ls[1], ls[2], ls[3], ls[4], ls[5]);
+        }
+
+        public static (T, T, T, T, T, T, T) Sort<T>(this (T, T, T, T, T, T, T) tuple7) where T : IComparable<T> {
+            var ls = tuple7.ToArray().ToList();
+            ls.Sort();
+            return (ls[0], ls[1], ls[2], ls[3], ls[4], ls[5], ls[6]);
+        }
+
+        #endregion
     }
 }
