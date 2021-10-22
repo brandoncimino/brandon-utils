e0e314162b438e9c3e8966442f421065f2cd615a
brandoncimino <brandon.cimino@gmail.com>
2021-10-22 16:58:00 -0500

    - Added `Tuple(T,T).Lerp()` extensions

--- Packages/BrandonUtils/Standalone/Mathb.Lerp.cs
+++ Packages/BrandonUtils/Standalone/Mathb.Lerp.cs
@@ -1,7 +1,39 @@
 ﻿// ReSharper disable UseDeconstructionOnParameter
 
+using System;
+
+using BrandonUtils.Standalone.Chronic;
+
 namespace BrandonUtils.Standalone {
     public static partial class Mathb {
+        public static int LerpInt(int start, int finish, double lerpAmount) {
+            if (lerpAmount <= 0) {
+                return start;
+            }
+            else if (lerpAmount >= 1) {
+                return finish;
+            }
+            else {
+                var dist   = finish - start;
+                var amount = (lerpAmount * dist).ToInt();
+                return start + amount;
+            }
+        }
+
+        public static long LerpInt(long start, long finish, double lerpAmount) {
+            if (lerpAmount <= 0) {
+                return start;
+            }
+            else if (lerpAmount >= 1) {
+                return finish;
+            }
+            else {
+                var dist   = finish - start;
+                var amount = (lerpAmount * dist).ToLong();
+                return start + amount;
+            }
+        }
+
         public static float Lerp(float start, float finish, float lerpAmount) {
             if (lerpAmount <= 0) {
                 return start;
@@ -44,8 +76,39 @@ namespace BrandonUtils.Standalone {
             }
         }
 
-        public static float   Lerp(this (float start, float finish)     range, float   lerpAmount) => Lerp(range.start, range.finish, lerpAmount);
-        public static double  Lerp(this (double start, double finish)   range, double  lerpAmount) => Lerp(range.start, range.finish, lerpAmount);
-        public static decimal Lerp(this (decimal start, decimal finish) range, decimal lerpAmount) => Lerp(range.start, range.finish, lerpAmount);
+        public static TimeSpan Lerp(TimeSpan start, TimeSpan finish, double lerpAmount) {
+            var ticks = LerpInt(start.Ticks, finish.Ticks, lerpAmount);
+            return TimeSpan.FromTicks(ticks);
+        }
+
+        public static DateTime Lerp(DateTime start, DateTime finish, double lerpAmount) {
+            if (lerpAmount <= 0) {
+                return start;
+            }
+            else if (lerpAmount >= 1) {
+                return finish;
+            }
+            else {
+                var dist   = finish - start;
+                var amount = dist.Multiply(lerpAmount);
+                return start + amount;
+            }
+        }
+
+        public static int      LerpInt(this (int start, int finish)           range, double  lerpAmount) => LerpInt(range.start, range.finish, lerpAmount);
+        public static long     LerpInt(this (long start, long finish)         range, double  lerpAmount) => LerpInt(range.start, range.finish, lerpAmount);
+        public static float    Lerp(this    (float start, float finish)       range, float   lerpAmount) => Lerp(range.start, range.finish, lerpAmount);
+        public static double   Lerp(this    (double start, double finish)     range, double  lerpAmount) => Lerp(range.start, range.finish, lerpAmount);
+        public static decimal  Lerp(this    (decimal start, decimal finish)   range, decimal lerpAmount) => Lerp(range.start, range.finish, lerpAmount);
+        public static TimeSpan Lerp(this    (TimeSpan start, TimeSpan finish) range, double  lerpAmount) => Lerp(range.start, range.finish, lerpAmount);
+        public static DateTime Lerp(this    (DateTime start, DateTime finish) range, double  lerpAmount) => Lerp(range.start, range.finish, lerpAmount);
+
+        #region Inverse Lerp
+
+        public static float InverseLerp(float start, float finish, float lerpedAmount) {
+            throw new NotImplementedException("STOOOOOOOOOP!");
+        }
+
+        #endregion
     }
 }
