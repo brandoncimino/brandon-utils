834a4d10dbcb8e3739360d0cc8271abf45b6143c
brandoncimino <brandon.cimino@gmail.com>
2021-10-24 19:51:09 -0500

    Utilized correct `ToObjArray` methods in `InnerPretty.Tuple`

--- Packages/BrandonUtils/Standalone/Strings/Prettifiers/InnerPretty.Tuple.cs
+++ Packages/BrandonUtils/Standalone/Strings/Prettifiers/InnerPretty.Tuple.cs
@@ -19,24 +19,24 @@ namespace BrandonUtils.Standalone.Strings.Prettifiers {
         }
 
         [NotNull, Pure]
-        public static string Tuple2<T1, T2>((T1, T2) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
+        public static string Tuple2<T1, T2>((T1, T2) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToObjArray().PrettifyTupleArray(settings);
 
         [NotNull, Pure]
-        public static string Tuple3<T, T2, T3>((T, T2, T3) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
+        public static string Tuple3<T, T2, T3>((T, T2, T3) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToObjArray().PrettifyTupleArray(settings);
 
         [NotNull, Pure]
-        public static string Tuple4<T, T2, T3, T4>((T, T2, T3, T4) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
+        public static string Tuple4<T, T2, T3, T4>((T, T2, T3, T4) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToObjArray().PrettifyTupleArray(settings);
 
         [NotNull, Pure]
-        public static string Tuple5<T, T2, T3, T4, T5>((T, T2, T3, T4, T5) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
+        public static string Tuple5<T, T2, T3, T4, T5>((T, T2, T3, T4, T5) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToObjArray().PrettifyTupleArray(settings);
 
         [NotNull, Pure]
-        public static string Tuple6<T, T2, T3, T4, T5, T6>((T, T2, T3, T4, T5, T6) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
+        public static string Tuple6<T, T2, T3, T4, T5, T6>((T, T2, T3, T4, T5, T6) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToObjArray().PrettifyTupleArray(settings);
 
         [NotNull, Pure]
-        public static string Tuple7<T, T2, T3, T4, T5, T6, T7>((T, T2, T3, T4, T5, T6, T7) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
+        public static string Tuple7<T, T2, T3, T4, T5, T6, T7>((T, T2, T3, T4, T5, T6, T7) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToObjArray().PrettifyTupleArray(settings);
 
         [NotNull, Pure]
-        public static string Tuple8Plus<T, T2, T3, T4, T5, T6, T7, T8>((T, T2, T3, T4, T5, T6, T7, T8) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToArray().PrettifyTupleArray(settings);
+        public static string Tuple8Plus<T, T2, T3, T4, T5, T6, T7, T8>((T, T2, T3, T4, T5, T6, T7, T8) tuple, [CanBeNull] PrettificationSettings settings = default) => tuple.ToObjArray().PrettifyTupleArray(settings);
     }
 }
