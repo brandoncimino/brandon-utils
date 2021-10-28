9203be81ae5a76510bb2744461f49f8464c7b2f4
brandoncimino <brandon.cimino@gmail.com>
2021-10-27 22:34:00 -0500

    Expanded `MethodTimer` overloads

--- Packages/BrandonUtils/Standalone/Chronic/MethodTimer.cs
+++ Packages/BrandonUtils/Standalone/Chronic/MethodTimer.cs
@@ -1,60 +1,220 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 using BrandonUtils.Standalone.Optional;
+using BrandonUtils.Standalone.Strings;
 
 using JetBrains.Annotations;
 
 namespace BrandonUtils.Standalone.Chronic {
+    [SuppressMessage("ReSharper", "UseDeconstructionOnParameter")]
     public static class MethodTimer {
-        [NotNull]
-        public static ExecutionTime MeasureExecution([NotNull] Action actionBeingTimed) {
-            return MeasureExecution(actionBeingTimed, new Stopwatch());
-        }
+        #region Single Execution
+
+        #region Action (0 args)
+
+        [NotNull] public static ExecutionTime MeasureExecution([NotNull, InstantHandle] Action actionBeingTimed) => MeasureExecution(default, actionBeingTimed);
+
+        [NotNull] public static ExecutionTime MeasureExecution([CanBeNull] string nickname, [NotNull, InstantHandle] Action actionBeingTimed) => MeasureExecution(nickname, actionBeingTimed, new Stopwatch());
 
         [NotNull]
-        private static ExecutionTime MeasureExecution([NotNull] Action actionBeingTimed, [NotNull] Stopwatch stopwatch) {
+        private static ExecutionTime MeasureExecution([CanBeNull] string nickname, [NotNull] Action actionBeingTimed, [NotNull] Stopwatch stopwatch) {
             stopwatch.Stop();
             stopwatch.Reset();
             stopwatch.Start();
             var result = actionBeingTimed.Try();
             stopwatch.Stop();
-            return new ExecutionTime(result, stopwatch.Elapsed);
+            return new ExecutionTime(nickname, result, stopwatch.Elapsed);
+        }
+
+        #endregion
+
+        #region Action<T> (1 arg)
+
+        [NotNull]
+        public static ExecutionTime MeasureExecution<T>(
+            [CanBeNull] string nickname,
+            [CanBeNull] T      input,
+            [NotNull, InstantHandle]
+            Action<T> actionBeingTimed
+        ) {
+            return MeasureExecution(nickname ?? actionBeingTimed.Method.Name, () => actionBeingTimed.Invoke(input));
         }
 
         [NotNull]
-        public static AggregateExecutionTime MeasureExecution([NotNull] Action action, [NonNegativeValue] int iterations) {
-            if (action == null) {
-                throw new ArgumentNullException(nameof(action));
+        public static ExecutionTime MeasureExecution<T>(
+            [CanBeNull] T input,
+            [NotNull, InstantHandle]
+            Action<T> actionBeingTimed
+        ) {
+            return MeasureExecution(default, input, actionBeingTimed);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Multiple Executions
+
+        #region Action (0 args)
+
+        [NotNull] public static AggregateExecutionTime MeasureExecution([NotNull] Action action, [NonNegativeValue] int iterations) => MeasureExecution((default, action), iterations);
+
+        [NotNull]
+        public static AggregateExecutionTime MeasureExecution((string nickname, Action action) actionBeingTimed, [NonNegativeValue] int iterations) {
+            if (actionBeingTimed.action == null) {
+                throw new ArgumentNullException(nameof(actionBeingTimed));
             }
 
             if (iterations <= 0) {
                 throw new ArgumentOutOfRangeException(nameof(iterations), iterations, "Must be > 0");
             }
 
+            actionBeingTimed.nickname = actionBeingTimed.nickname.IfBlank(actionBeingTimed.action.Method.Name);
+
             var results = new ExecutionTime[iterations];
             for (int i = 0; i < iterations; i++) {
-                results[i] = MeasureExecution(action);
+                results[i] = MeasureExecution(actionBeingTimed.nickname, actionBeingTimed.action);
             }
 
-            return new AggregateExecutionTime(results);
+            return new AggregateExecutionTime(actionBeingTimed.nickname, results);
         }
 
-        public static AggregateExecutionComparison CompareExecutions([NotNull] Action firstAction, [NotNull] Action secondAction, int iterations) {
-            if (firstAction == null) {
-                throw new ArgumentNullException(nameof(firstAction));
+        #endregion
+
+        #region Action<T> (1 arg)
+
+        [NotNull]
+        public static AggregateExecutionTime MeasureExecution<T>(
+            (string nickname, T input, Action<T> action) actionBeingTimed,
+            [NonNegativeValue]
+            int iterations
+        ) {
+            return MeasureExecution((actionBeingTimed.nickname ?? actionBeingTimed.action.Method.Name, () => actionBeingTimed.action(actionBeingTimed.input)), iterations);
+        }
+
+        [NotNull]
+        public static AggregateExecutionTime MeasureExecution<T>(
+            [CanBeNull] T         input,
+            [NotNull]   Action<T> actionBeingTimed,
+            [NonNegativeValue]
+            int iterations
+        ) {
+            return MeasureExecution((default, input, actionBeingTimed), iterations);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Comparisons
+
+        #region Action (0 args)
+
+        [NotNull]
+        public static AggregateExecutionComparison CompareExecutions(
+            (string nickname, Action action) first,
+            (string nickname, Action action) second,
+            int                              iterations
+        ) {
+            if (first.action == null) {
+                throw new ArgumentNullException($"{nameof(first)}.{nameof(first.action)}");
             }
 
-            if (secondAction == null) {
-                throw new ArgumentNullException(nameof(secondAction));
+            if (second.action == null) {
+                throw new ArgumentNullException($"{nameof(second)}.{nameof(second.action)}");
             }
 
-            var firstTimes  = MeasureExecution(firstAction,  iterations);
-            var secondTimes = MeasureExecution(secondAction, iterations);
+            var firstTimes  = MeasureExecution(first,  iterations);
+            var secondTimes = MeasureExecution(second, iterations);
 
             var comparison = new AggregateExecutionComparison(firstTimes, secondTimes);
             Console.WriteLine(comparison);
             return comparison;
         }
+
+        [NotNull]
+        public static AggregateExecutionComparison CompareExecutions([NotNull] Action firstAction, [NotNull] Action secondAction, int iterations) {
+            return CompareExecutions(
+                (default, firstAction),
+                (default, secondAction),
+                iterations
+            );
+        }
+
+        #endregion
+
+        #region Action<T> (1 arg)
+
+        [NotNull]
+        public static AggregateExecutionComparison CompareExecutions<T>(
+            [CanBeNull] T                       input,
+            (string nickname, Action<T> action) first,
+            (string nickname, Action<T> action) second,
+            [NonNegativeValue]
+            int iterations
+        ) {
+            return CompareExecutions(
+                (first.nickname, () => first.action(input)),
+                (second.nickname, () => second.action(input)),
+                iterations
+            );
+        }
+
+        [NotNull]
+        public static AggregateExecutionComparison CompareExecutions<T>(
+            [CanBeNull] T         input,
+            [NotNull]   Action<T> first,
+            [NotNull]   Action<T> second,
+            [NonNegativeValue]
+            int iterations
+        ) {
+            return CompareExecutions(
+                input,
+                (default, first),
+                (default, second),
+                iterations
+            );
+        }
+
+        #endregion
+
+        #region Action<T1,T2> (2 args)
+
+        [NotNull]
+        public static AggregateExecutionComparison CompareExecutions<T1, T2>(
+            (T1 arg1, T2 arg2)                       input,
+            (string nickname, Action<T1, T2> action) first,
+            (string nickname, Action<T1, T2> action) second,
+            [NonNegativeValue]
+            int iterations
+        ) {
+            return CompareExecutions(
+                (first.nickname, () => first.action.Invoke(input)),
+                (second.nickname, () => second.action.Invoke(input)),
+                iterations
+            );
+        }
+
+        [NotNull]
+        public static AggregateExecutionComparison CompareExecutions<T1, T2>(
+            (T1 arg1, T2 arg2)       input,
+            [NotNull] Action<T1, T2> first,
+            [NotNull] Action<T1, T2> second,
+            [NonNegativeValue]
+            int iterations
+        ) {
+            return CompareExecutions(
+                input,
+                (default, first),
+                (default, second),
+                iterations
+            );
+        }
+
+        #endregion
+
+        #endregion
     }
 }
