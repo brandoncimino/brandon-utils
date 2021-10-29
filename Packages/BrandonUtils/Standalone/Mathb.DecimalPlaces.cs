58485ab720d3323ddd961b53a717732837b1f131
brandoncimino <brandon.cimino@gmail.com>
2021-10-28 20:33:20 -0500

    - Re-added `ToPercentage` - Modified `float.ShiftDecimal()` to return a `float` instead of a `double`

--- Packages/BrandonUtils/Standalone/Mathb.DecimalPlaces.cs
+++ Packages/BrandonUtils/Standalone/Mathb.DecimalPlaces.cs
@@ -9,6 +9,9 @@ namespace BrandonUtils.Standalone {
         [Pure] public static double ShiftDecimal(this uint   value, int decimalPlaces) => ShiftDecimal(value.ToDouble(), decimalPlaces);
         [Pure] public static double ShiftDecimal(this short  value, int decimalPlaces) => ShiftDecimal(value.ToDouble(), decimalPlaces);
         [Pure] public static double ShiftDecimal(this ushort value, int decimalPlaces) => ShiftDecimal(value.ToDouble(), decimalPlaces);
-        [Pure] public static double ShiftDecimal(this float  value, int decimalPlaces) => ShiftDecimal(value.ToDouble(), decimalPlaces);
+        [Pure] public static float  ShiftDecimal(this float  value, int decimalPlaces) => value == 0 ? 0 : value * Math.Pow(10, decimalPlaces).ToFloat();
+
+        [Pure] public static double ToPercentage(this double value) => value.ShiftDecimal(2);
+        [Pure] public static float  ToPercentage(this float  value) => value.ShiftDecimal(2);
     }
 }
