b06a58ed52636167dee06452e68ac1f9d4013c57
brandoncimino <brandon.cimino@gmail.com>
2021-10-28 21:14:54 -0500

    Added `Mathb.Abs()`

---
+++ Packages/BrandonUtils/Standalone/Mathb.Abs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BrandonUtils.Standalone {
+    public static partial class Mathb {
+        public static sbyte  Abs(this sbyte  value) => Math.Abs(value);
+        public static short  Abs(this short  value) => Math.Abs(value);
+        public static int    Abs(this int    value) => Math.Abs(value);
+        public static long   Abs(this long   value) => Math.Abs(value);
+        public static double Abs(this double value) => Math.Abs(value);
+        public static float  Abs(this float  value) => Math.Abs(value);
+    }
+}
