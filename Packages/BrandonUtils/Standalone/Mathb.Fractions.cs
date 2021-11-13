656d1eec908cd76dab030bab50c7853d133433b7
brandoncimino <brandon.cimino@gmail.com>
2021-11-12 22:07:09 -0600

    - Added `Mathb.Fraction()` - Added `Mathb.Apportion()`

--- Packages/BrandonUtils/Standalone/Mathb.Fractions.cs
+++ Packages/BrandonUtils/Standalone/Mathb.Fractions.cs
@@ -25,5 +25,34 @@ namespace BrandonUtils.Standalone {
 
         [Pure] public static double DeltaRatio(Rate     initial, Rate     final) => DeltaRatio(initial.Hertz, final.Hertz);
         [Pure] public static double DeltaRatio(TimeSpan initial, TimeSpan final) => DeltaRatio(initial.Ticks, final.Ticks);
+
+        #region Fraction (i.e. the "fractional part")
+
+        public static float   Fraction(this float   value) => value % 1;
+        public static double  Fraction(this double  value) => value % 1;
+        public static decimal Fraction(this decimal value) => value % 1;
+
+        #endregion
+
+        #region Apportion
+
+        public static (double a, double b) Apportion(double a, double b, double total = 1) {
+            var sum      = a + b;
+            var aRatio   = a      / sum;
+            var aPortion = aRatio * total;
+            var bRatio   = total - aPortion;
+            return (aRatio, bRatio);
+        }
+
+        public static (int a, int b) Apportion(double a, double b, int total) {
+            var sum      = a + b;
+            var aRatio   = a      / sum;
+            var aPortion = aRatio * total;
+            var aRounded = Math.Round(aPortion).ToInt();
+            var bRounded = total - aRounded;
+            return (aRounded, bRounded);
+        }
+
+        #endregion
     }
 }
