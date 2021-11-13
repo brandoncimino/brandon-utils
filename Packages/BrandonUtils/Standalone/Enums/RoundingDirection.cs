aaa78a303d8803226e88c3b38ea71b543a6942a2
brandoncimino <brandon.cimino@gmail.com>
2021-11-12 22:01:31 -0600

    Added a `RoundingDirection` enum (`Ceiling` or `Floor`)

---
+++ Packages/BrandonUtils/Standalone/Enums/RoundingDirection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BrandonUtils.Standalone.Enums {
+    /// <summary>
+    /// Decides whether to round using <see cref="Math.Ceiling(decimal)"/> or <see cref="Math.Floor(decimal)"/>.
+    /// </summary>
+    /// <remarks>
+    /// Sort of a complement to <see cref="MidpointRounding"/>?
+    /// </remarks>
+    public enum RoundingDirection {
+        /// <summary>
+        /// AKA <see cref="Math.Ceiling(decimal)"/>
+        /// </summary>
+        Ceiling,
+        /// <summary>
+        /// AKA <see cref="Math.Floor(decimal)"/>
+        /// </summary>
+        Floor
+    }
+}
