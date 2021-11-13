e8fe7febd3b3f3c44121ca26c3f9cccb3b2fcdb2
brandoncimino <brandon.cimino@gmail.com>
2021-11-12 21:59:15 -0600

    Added `IEnumSet`, but I don't really know why; it just kind of feels..._professional_?

---
+++ Packages/BrandonUtils/Standalone/Enums/IEnumSet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BrandonUtils.Standalone.Enums {
+    /// <summary>
+    /// A specialized <see cref="ISet{T}"/> that contains <see cref="Enum"/> values.
+    /// </summary>
+    /// <typeparam name="T">the <see cref="Enum"/> type of the set's members</typeparam>
+    public interface IEnumSet<T> : ISet<T> where T : struct, Enum {
+        public IEnumSet<T> Inverse();
+    }
+}
