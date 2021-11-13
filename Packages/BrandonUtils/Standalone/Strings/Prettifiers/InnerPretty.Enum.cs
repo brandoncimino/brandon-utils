7af08db26180f69e62573671de9e326b8193650a
brandoncimino <brandon.cimino@gmail.com>
2021-11-12 22:02:56 -0600

    Added InnerPretty.Enum.cs

---
+++ Packages/BrandonUtils/Standalone/Strings/Prettifiers/InnerPretty.Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using JetBrains.Annotations;
+
+namespace BrandonUtils.Standalone.Strings.Prettifiers {
+    internal partial class InnerPretty {
+        internal static string PrettifyEnum([NotNull] Enum enm, [CanBeNull] PrettificationSettings settings = default) {
+            settings ??= Prettification.DefaultPrettificationSettings;
+            return settings.TypeLabelStyle.Value switch {
+                TypeNameStyle.None => enm.ToString(),
+                _                  => $"{enm.GetType().Name}.{enm}"
+            };
+        }
+    }
+}
