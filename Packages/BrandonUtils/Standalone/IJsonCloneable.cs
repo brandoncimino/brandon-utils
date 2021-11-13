42086aee781a95915753afa165415faa5c9ac73b
brandoncimino <brandon.cimino@gmail.com>
2021-11-12 22:08:34 -0600

    - Added (experimental) `IJsonCloneable`

---
+++ Packages/BrandonUtils/Standalone/IJsonCloneable.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+
+using Newtonsoft.Json;
+
+namespace BrandonUtils.Standalone {
+    public interface IJsonCloneable { }
+
+    public static class JsonCloneableExtensions {
+        [CanBeNull]
+        [ContractAnnotation("original:null => null")]
+        [ContractAnnotation("original:notnull => notnull")]
+        public static T JsonClone<T>([CanBeNull] this T original, [CanBeNull] JsonSerializerSettings settings = default) where T : IJsonCloneable {
+            var json = JsonConvert.SerializeObject(original, settings!);
+            return JsonConvert.DeserializeObject<T>(json, settings);
+        }
+    }
+}
