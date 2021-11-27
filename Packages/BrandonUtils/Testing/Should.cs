822321057ffd1c78259873a9b7610fc331bd135c
brandoncimino <brandon.cimino@gmail.com>
2021-11-27 17:59:55 -0600

    Added `Should.BeNull` and `Should.BeNotNull`

--- Packages/BrandonUtils/Testing/Should.cs
+++ Packages/BrandonUtils/Testing/Should.cs
@@ -2,12 +2,39 @@
 
 using BrandonUtils.Standalone.Enums;
 
+using NUnit.Framework;
 using NUnit.Framework.Constraints;
 
 namespace BrandonUtils.Testing {
+    /// <summary>
+    /// Enum values that correspond to common NUnit <see cref="Constraint"/>s such as <see cref="NUnit.Framework.Is"/>.<see cref="NUnit.Framework.Is.Null"/>,
+    /// enabling them to be referenced in <see cref="NUnit.Framework.TestCaseAttribute"/>s
+    /// which only accept constant values.
+    /// </summary>
     public enum Should {
+        /// <summary>
+        /// Corresponds to <see cref="NUnit.Framework.Is"/>.<see cref="NUnit.Framework.Is.True"/> (i.e. <see cref="TrueConstraint"/>).
+        /// </summary>
+        /// <remarks>
+        /// TODO: Change this to represent <see cref="Throws.Nothing"/>, and create <c>Should.BeTrue</c> to represent <see cref="NUnit.Framework.Is.True"/>
+        ///     This is too ambiguous, and it implies that the test method itself is expected to throw an exception, like some other test frameworks do.
+        /// </remarks>
         Pass,
-        Fail
+        /// <summary>
+        /// Corresponds to <see cref="NUnit.Framework.Is"/>.<see cref="NUnit.Framework.Is.False"/> (i.e. <see cref="FalseConstraint"/>).
+        /// </summary>
+        /// <remarks>
+        /// TODO: Change this to represent <see cref="Throws.Exception"/>, and create <c>Should.BeFalse</c>
+        /// </remarks>
+        Fail,
+        /// <summary>
+        /// Corresponds to <see cref="NUnit.Framework.Is"/>.<see cref="NUnit.Framework.Is.Null"/> (i.e. <see cref="NullConstraint"/>).
+        /// </summary>
+        BeNull,
+        /// <summary>
+        /// Corresponds to <see cref="NUnit.Framework.Is"/>.<see cref="NUnit.Framework.Is.Not"/>.<see cref="ConstraintExpression.Null"/>.
+        /// </summary>
+        BeNotNull,
     }
 
     [SuppressMessage("ReSharper", "AccessToStaticMemberViaDerivedType")]
@@ -22,9 +49,11 @@ namespace BrandonUtils.Testing {
 
         public static Constraint Constrain(this Should should) {
             return should switch {
-                Should.Pass => Is.True,
-                Should.Fail => Is.False,
-                _           => throw BEnum.InvalidEnumArgumentException(nameof(should), should)
+                Should.Pass      => Is.True,
+                Should.Fail      => Is.False,
+                Should.BeNull    => Is.Null,
+                Should.BeNotNull => Is.Not.Null,
+                _                => throw BEnum.InvalidEnumArgumentException(nameof(should), should)
             };
         }
     }
