3e0f24c1a7fef6fba59364d68c0c5c122aa5fc0c
brandoncimino <brandon.cimino@gmail.com>
2021-12-03 21:56:02 -0600

    Passed `PrettifyEnum` through `WithTypeLabel`

--- Packages/BrandonUtils/Standalone/Strings/Prettifiers/InnerPretty.Enum.cs
+++ Packages/BrandonUtils/Standalone/Strings/Prettifiers/InnerPretty.Enum.cs
@@ -4,12 +4,10 @@ using JetBrains.Annotations;
 
 namespace BrandonUtils.Standalone.Strings.Prettifiers {
     internal partial class InnerPretty {
-        internal static string PrettifyEnum([NotNull] Enum enm, [CanBeNull] PrettificationSettings settings = default) {
-            settings ??= Prettification.DefaultPrettificationSettings;
-            return settings.TypeLabelStyle.Value switch {
-                TypeNameStyle.None => enm.ToString(),
-                _                  => $"{enm.GetType().Name}.{enm}"
-            };
+        [NotNull]
+        internal static string PrettifyEnum([NotNull] Enum enm, [NotNull] PrettificationSettings settings) {
+            settings = Prettification.ResolveSettings(settings);
+            return enm.ToString().WithTypeLabel(enm.GetType(), settings, ".");
         }
     }
 }
