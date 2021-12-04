792a6bac5b47564558767ee69f3bb754e2113dd3
brandoncimino <brandon.cimino@gmail.com>
2021-12-03 22:00:35 -0600

    improved handling of enum type labels

--- Packages/BrandonUtils/Standalone/Strings/TypeNameStyle.cs
+++ Packages/BrandonUtils/Standalone/Strings/TypeNameStyle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using BrandonUtils.Standalone.Reflection;
+using BrandonUtils.Standalone.Strings.Json;
 using BrandonUtils.Standalone.Strings.Prettifiers;
 
 using JetBrains.Annotations;
@@ -15,24 +16,23 @@ namespace BrandonUtils.Standalone.Strings {
     public static class TypeNameStyleExtensions {
         [NotNull]
         public static string GetTypeLabel([CanBeNull] this Type type, [CanBeNull] PrettificationSettings settings) {
-            settings ??= Prettification.DefaultPrettificationSettings;
+            settings = Prettification.ResolveSettings(settings);
 
-            if (type == null || settings.TypeLabelStyle == TypeNameStyle.None) {
+            var style = type?.IsEnum == true ? settings.EnumLabelStyle : settings.TypeLabelStyle;
+
+            settings?.TraceWriter.Verbose(() => $"Using style: {style} (type: {settings.TypeLabelStyle}/{settings.TypeLabelStyle.Value}, enum: {settings.EnumLabelStyle}/{settings.EnumLabelStyle.Value})");
+
+            if (type == null || style == TypeNameStyle.None) {
                 return "";
             }
 
             var str = type.PrettifyType(settings);
 
-            if (type.IsArray || type.IsEnumerable()) {
+            if (type.IsArray || type.IsEnum || type.IsEnumerable()) {
                 return str;
             }
 
             return $"[{str}]";
-        }
-
-        [NotNull]
-        public static string WithTypeLabel([CanBeNull] string thing, [CanBeNull] Type type, [CanBeNull] PrettificationSettings settings) {
-            return $"{GetTypeLabel(type, settings)}{thing.OrNullPlaceholder(settings)}";
         }
     }
 }
